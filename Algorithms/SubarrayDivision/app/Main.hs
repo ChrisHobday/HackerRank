6f54a604972c99e156f75960824e28f6fafb4e58
Chris Hobday <hobday.chris@protonmail.com>
2021-10-30 16:11:11 -0400

    Complete SubarrayDivision challenge

--- Algorithms/SubarrayDivision/app/Main.hs
+++ Algorithms/SubarrayDivision/app/Main.hs
@@ -1,11 +1,19 @@
 module Main where
 
-import Data.List ( group, sort, subsequences )
-
--- Remove duplicate occurences in a given list
-removeDups :: Ord a => [a] -> [a]
-removeDups = map head . group . sort
+-- Calculate the number of contiguous segments where the length = Ron's birth month and the sum of the integers on = Ron's birth day
+birthday :: [Int] -> Int -> Int -> Int
+birthday [] _ _ = 0
+birthday s d m
+  | sum (take m s) == d = 1 + birthday (tail s) d m
+  | otherwise           = birthday (tail s) d m
 
 main :: IO ()
 main = do
-  putStrLn ""
+  _ <- readLn :: IO Int -- Read Line containing number of squares to be entered
+  sTemp <- getLine -- Read line containing chocolate squares with integers on
+  dMTemp <- getLine -- Read line Ron's birth day and month
+  let s  = map (read :: String -> Int) (words sTemp) -- Convert read String to list of Ints
+      dM = map (read :: String -> Int) (words dMTemp) -- Convert read String to list of Ints
+      d  = head dM -- Set birth day
+      m  = last dM -- Set birth month
+  print $ birthday s d m -- Print the number of contiguous segments where the length = Ron's birth month and the sum of the integers on = Ron's birth day
