317531739b2abc4eecf647a886716797685d1610
Chris Hobday <hobday.chris@protonmail.com>
2021-11-01 19:45:27 -0400

    Complete MigratoryBirds challenge

--- Algorithms/MigratoryBirds/app/Main.hs
+++ Algorithms/MigratoryBirds/app/Main.hs
@@ -11,10 +11,13 @@ birdSightings _ [] = []
 birdSightings 0 _  = []
 birdSightings a bs = (a, numOccurences a bs) : birdSightings (a - 1) bs
 
-migratoryBirds as = undefined
+-- The bird with the most sightings from a given list of bird sightings
+migratoryBirds :: [Int] -> Int
+migratoryBirds as = fst $ last $ filter ((== maximum (map snd (birdSightings 5 as))) . snd) (birdSightings 5 as)
 
 main :: IO ()
 main = do
+  _ <- readLn :: IO Int -- Read Line containing number of bird sightings to be entered
   birdsTemp <- getLine -- Read line containing bird sightings
   let birds  = map (read :: String -> Int) (words birdsTemp) -- Convert read String to list of Ints
-  print $ birds
+  print $ migratoryBirds birds -- Print the bird with the most sightings
