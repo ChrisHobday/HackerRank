d24c5c1421cbe6dae9042c5d7f31999e5c34d0e3
Chris Hobday <hobday.chris@protonmail.com>
2021-11-02 05:00:42 -0400

    Change HelloWorld to use 'putStrLn String' instead of 'print $ NoQuotes String'

--- Functional Programming/HelloWorld/app/Main.hs
+++ Functional Programming/HelloWorld/app/Main.hs
@@ -1,10 +1,7 @@
 module Main where
 
-newtype NoQuotes = NoQuotes String
-instance Show NoQuotes where show (NoQuotes string) = string
-
 helloWorld :: String
 helloWorld = "Hello World"
 
 main :: IO ()
-main = print $ NoQuotes helloWorld
+main = putStrLn helloWorld
