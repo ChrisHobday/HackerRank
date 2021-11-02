c2a4e7ffe7d62800411a70058d873b6d10d74f53
Chris Hobday <hobday.chris@protonmail.com>
2021-11-02 19:38:47 -0400

    Complete DrawingBook challenge

---
+++ Algorithms/DrawingBook/ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for DrawingBook
+
+## Unreleased changes
---
+++ Algorithms/DrawingBook/app/Main.hs
@@ -0,0 +1,13 @@
+module Main where
+
+-- The minimum number of pages to turn given the number of pages and page to turn to
+pageCount nPages page
+  | (nPages `div` 2) >= page = page `div` 2
+  | otherwise                = (nPages `div` 2) - (page `div` 2)
+
+main :: IO ()
+main = do
+  nPages <- readLn :: IO Int -- Read number of pages
+  page <- readLn :: IO Int -- Read page to turn to
+  print $ pageCount nPages page -- Print the minimum number of pages to turn
+  -- print $ min (page `div` 2) (nPages `div` 2 - page `div` 2) -- Print the minimum number of page turns
