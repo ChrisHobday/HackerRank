baf21208753a7f7518aa500ab3cc180d7e13ddf5
Chris Hobday <hobday.chris@protonmail.com>
2021-11-12 22:29:26 -0500

    Complete PickingNumbers challenge

--- Algorithms/PickingNumbers/app/Main.hs
+++ Algorithms/PickingNumbers/app/Main.hs
@@ -1,13 +1,18 @@
 module Main where
 
-x = [1,1,2,2,4,4,5,5,5]
+import Data.List ( subsequences )
 
-pickingNumbers []      = []
-pickingNumbers numbers = length (fst subList) : pickingNumbers (snd subList)
-                           where subList = span (<= head numbers + 1) numbers
+-- A list of Bools representing whether the absolute difference between numbers in a given list is <= 1
+isDifferenceOf1 :: (Ord a, Num a) => [a] -> [Bool]
+isDifferenceOf1 []     = []
+isDifferenceOf1 (a:as) = map ((<= 1) . abs . (-) a) as ++ isDifferenceOf1 as
+
+-- The maximum length subsequence of a given list of numbers where each number in the subsequence is within 1 of each other number
+pickingNumbers :: (Ord a, Num a) => [a] -> Int
+pickingNumbers numbers = maximum $ map length $ filter ((==) True . and . isDifferenceOf1) (subsequences numbers)
 
 main :: IO ()
 main = do
-  _ <- readLn :: IO Int
-  numbers <- map read . words <$> getLine :: IO [Int]
-  print $ maximum $ pickingNumbers numbers
+  _ <- readLn :: IO Int -- Read the number of numbers to be entered but don't bind it because we don't need it
+  numbers <- map read . words <$> getLine :: IO [Int] -- Read list of numbers and bind it
+  print $ pickingNumbers numbers -- Print the maximum length subsequence of the given list of numbers where each number in the subsequence is within 1 of each other number
