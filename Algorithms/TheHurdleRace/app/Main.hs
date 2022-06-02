9ace8bdd28db7c47ab31fce842189ae2a7212fb8
Chris Hobday <hobday.chris@protonmail.com>
2022-06-02 14:29:06 -0400

    Add starting skeleton code

--- Algorithms/TheHurdleRace/app/Main.hs
+++ Algorithms/TheHurdleRace/app/Main.hs
@@ -1,5 +1,11 @@
 module Main where
 
+hurdleRace :: Int -> [Int] -> Int
+hurdleRace k height = undefined
+    -- Write your code here
+
 main :: IO ()
 main = do
-  return ()
+  maximumJumpHeight <- read . last . words <$> getLine :: IO Int -- Read maximum height the character can jump
+  hurdleHeights <- map read . words <$> getLine :: IO [Int] -- Read list of player scores
+  print $ hurdleRace maximumJumpHeight hurdleHeights -- Print the number of doses of potion the character must take to be able to jump all of the hurdles
