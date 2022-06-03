53e99d1c76f1eff2ae9178a62243d23a9a8a11db
Chris Hobday <hobday.chris@protonmail.com>
2022-06-02 20:34:45 -0400

    Add starting skeleton code

--- Algorithms/ExtraLongFactorials/app/Main.hs
+++ Algorithms/ExtraLongFactorials/app/Main.hs
@@ -1,4 +1,9 @@
 module Main where
 
+extraLongFactorials :: (Eq a, Num a) => a -> a
+extraLongFactorials n = undefined
+
 main :: IO ()
-main = undefined
+main = do
+  n <- readLn :: IO Int
+  print $ extraLongFactorials n
