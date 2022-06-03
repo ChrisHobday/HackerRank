8d33fff4a4adadab4315148961b6cab5cb5a3a27
Chris Hobday <hobday.chris@protonmail.com>
2022-06-02 20:46:06 -0400

    Complete ExtraLongFactorials challenge

--- Algorithms/ExtraLongFactorials/app/Main.hs
+++ Algorithms/ExtraLongFactorials/app/Main.hs
@@ -1,9 +1,12 @@
 module Main where
 
+-- The factorial of a given number (workds for extra long numbers)
+-- Ex. extraLongFactorials 50 = 30414093201713378043612608166064768844377641568960512000000000000
 extraLongFactorials :: (Eq a, Num a) => a -> a
-extraLongFactorials n = undefined
+extraLongFactorials 0 = 1
+extraLongFactorials n = n * extraLongFactorials (n - 1)
 
 main :: IO ()
 main = do
-  n <- readLn :: IO Int
-  print $ extraLongFactorials n
+  n <- readLn :: IO Integer -- Read a number (stored with Integer type for arbitrary length)
+  print $ extraLongFactorials n -- Print the factorial of read number
