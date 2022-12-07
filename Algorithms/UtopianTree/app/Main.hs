f7b1400a7b3071b6356567ea942ef1abb2ff42ef
ChrisHobday <hobday.chris@protonmail.com>
2022-12-07 18:16:25 -0500

    Complete UtopianTree challenge

--- Algorithms/UtopianTree/app/Main.hs
+++ Algorithms/UtopianTree/app/Main.hs
@@ -3,10 +3,16 @@ module Main (main) where
 import Control.Monad ( replicateM )
 
 utopianTree :: Int -> Int
-utopianTree n = 0
+utopianTree growthCycles
+  -- No growth cycles (or an illegal negative amount)
+  | growthCycles <= 0 = 1 -- Default/starting height of 1
+  -- Even number of growth cycles
+  | even growthCycles = 1 + utopianTree (growthCycles - 1) -- Add 1 to height 
+  -- Otherwise (odd number of growth cycles)
+  | otherwise         = 2 * utopianTree (growthCycles - 1) -- Double height
 
 main :: IO ()
 main = do
   numberTestCases <- readLn :: IO Int -- Read number of test cases to be entered
-  testCases <- replicateM numberTestCases (readLn :: IO Int) -- Read list of test cases for growth cycles
-  print $ map utopianTree testCases
+  testCases <- replicateM numberTestCases (readLn :: IO Int) -- Read list of test cases for utopian trees
+  mapM_ (print . utopianTree) testCases -- Print utopian tree results for given utopian trees
