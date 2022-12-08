52da7ee9d0cd5fd01da7b178f81d8737767e334b
ChrisHobday <hobday.chris@protonmail.com>
2022-12-08 10:29:10 -0500

    Add problem statement, sample test cases and skeleton code

--- Algorithms/AngryProfessor/app/Main.hs
+++ Algorithms/AngryProfessor/app/Main.hs
@@ -1,6 +1,18 @@
 module Main (main) where
 
-import Lib
+import Control.Monad ( replicateM
+                     , replicateM_ )
+
+-- Whether or not the class is canceled depending on the cancelation threshold and arrival times of the students
+angryProfessor :: Int -> [Int] -> String
+angryProfessor cancelationThreshold arrivalTimes = "undefined"
 
 main :: IO ()
-main = someFunc
+main = do
+  numberOfTestCases <- readLn :: IO Int -- Read number of test cases to be entered and bind it
+  testCases <- replicateM numberOfTestCases $ -- Replicate the following action for each test case
+    do
+      (_: cancelationThreshold:_) <- map read . words <$> getLine :: IO [Int] -- Read the cancelation threshold and bind it (ignores first int entered and any after that)
+      arrivalTimes <- map read . words <$> getLine :: IO [Int] -- Read the list of arrival times and bind it
+      return (cancelationThreshold, arrivalTimes) -- Return the cancelation threshold and arrival times as a tuple
+  mapM_ (\(cancelationThreshold, arrivalTimes) -> print $ angryProfessor cancelationThreshold arrivalTimes) testCases -- Print which classes are canceled or not
