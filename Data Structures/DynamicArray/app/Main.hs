1ca36d47b8a2e53b9c8e51ea346bfd8542f85794
ChrisHobday <hobday.chris@protonmail.com>
2022-12-10 09:16:18 -0500

    Update old package.yamls. Add old problem statements and sample test cases

--- Data Structures/DynamicArray/app/Main.hs
+++ Data Structures/DynamicArray/app/Main.hs
@@ -12,20 +12,26 @@ array = [[],[]]
 lastAnswer :: Int
 lastAnswer = 0
 
-idx :: Int -> Int -> Int -> Int
-idx queryValue1 lastAnswer arraySize = (queryValue1 `xor` lastAnswer) `mod` arraySize
+idx :: Int -> Int -> Int
+idx x n = (x `xor` lastAnswer) `mod` n
+
+Query1 x y n arr = arr1:y:arr2
+  where (arr1, arr2) = splitAt (idx x n)
+
+-- idx :: Int -> Int -> Int -> Int
+-- idx queryValue1 lastAnswer arraySize = (queryValue1 `xor` lastAnswer) `mod` arraySize
 
 -- onceThrough :: Vector Int -> Int -> Int -> Int -> Int -> Vector Int
 -- onceThrough array queryValue1 lastAnswer arraySize queryValue2 = array // [(idx queryValue1 lastAnswer arraySize,queryValue2)]
 
-onceThrough :: [[Int]] -> Int -> Int -> Int -> Int -> [[Int]]
-onceThrough array queryValue1 lastAnswer arraySize queryValue2 = 
-  arraySplit1 ++ (yArray ++ [queryValue2]) : arraySplit2
-  where (arraySplit1,yArray:arraySplit2) = splitAt (idx queryValue1 lastAnswer arraySize) array
+-- onceThrough :: [[Int]] -> Int -> Int -> Int -> Int -> [[Int]]
+-- onceThrough array queryValue1 lastAnswer arraySize queryValue2 = 
+--   arraySplit1 ++ (yArray ++ [queryValue2]) : arraySplit2
+--   where (arraySplit1,yArray:arraySplit2) = splitAt (idx queryValue1 lastAnswer arraySize) array
 
-onceThrough2 :: [[Int]] -> Int -> Int -> Int -> Int -> Int
-onceThrough2 array queryValue1 lastAnswer arraySize queryValue2 = 
-  head $ array !! idx queryValue1 lastAnswer arraySize
+-- onceThrough2 :: [[Int]] -> Int -> Int -> Int -> Int -> Int
+-- onceThrough2 array queryValue1 lastAnswer arraySize queryValue2 = 
+--   head $ array !! idx queryValue1 lastAnswer arraySize
 
 main :: IO ()
 main = print ""
