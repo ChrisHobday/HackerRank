a6b84427d1a7920d7fb604586934aaf703392a87
ChrisHobday <hobday.chris@protonmail.com>
2022-12-16 15:29:10 -0500

    Add skeleton code

--- Data Structures/SparseArrays/app/Main.hs
+++ Data Structures/SparseArrays/app/Main.hs
@@ -1,5 +1,16 @@
 module Main (main) where
 
+import Control.Monad ( replicateM )
+
+matchingStrings stringList queries = ""
+
 main :: IO ()
 main = do
-  return ()
+  n <- readLn :: IO Int -- Read and bind number of strings to be entered
+  stringList <- replicateM n $ do -- For each string to be entered
+    getLine -- Get entered string
+  q <- readLn :: IO Int -- Read and bind number of queries to be entered
+  queries <- replicateM n $ do -- For each query to be entered
+    getLine -- Get entered query
+  putStrLn $ matchingStrings stringList queries
+  
