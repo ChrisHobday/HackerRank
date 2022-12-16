e1a48673c2054c1cc2817ce3d9db8ff058be9b9d
ChrisHobday <hobday.chris@protonmail.com>
2022-12-16 16:00:31 -0500

    Complete SparseArrays challenge

--- Data Structures/SparseArrays/app/Main.hs
+++ Data Structures/SparseArrays/app/Main.hs
@@ -2,7 +2,13 @@ module Main (main) where
 
 import Control.Monad ( replicateM )
 
-matchingStrings stringList queries = ""
+-- The number of matching query occurences in a given list of strings, formatted as a string with a number on each line
+matchingStrings :: Eq a => [a] -> [a] -> [Char]
+-- No strings
+matchingStrings [] _ = ""
+-- No queries
+matchingStrings _ [] = ""
+matchingStrings stringList queries@(q:qs) = show ((length . filter (== q)) stringList) ++ (if qs /= [] then "\n" else "") ++ matchingStrings stringList qs -- Add number of matching query occurences to string and check matching strings on the rest of the queries
 
 main :: IO ()
 main = do
@@ -10,7 +16,6 @@ main = do
   stringList <- replicateM n $ do -- For each string to be entered
     getLine -- Get entered string
   q <- readLn :: IO Int -- Read and bind number of queries to be entered
-  queries <- replicateM n $ do -- For each query to be entered
+  queries <- replicateM q $ do -- For each query to be entered
     getLine -- Get entered query
-  putStrLn $ matchingStrings stringList queries
-  
+  putStrLn $ matchingStrings stringList queries -- Print the number of matching strings between queries and string list
