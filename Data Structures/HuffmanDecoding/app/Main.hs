fe2d3f4bbdd519cb00d635b6cb51e78d4d1d62fd
ChrisHobday <hobday.chris@protonmail.com>
2022-12-27 21:25:05 -0500

    Update HuffmanDecoding challenge, redo countFirstAndRemove function, add countAll function

--- Data Structures/HuffmanDecoding/app/Main.hs
+++ Data Structures/HuffmanDecoding/app/Main.hs
@@ -1,17 +1,27 @@
 module Main (main) where
 
-countAndRemove [] _ = 0
-countAndRemove (firstChar:chars) char
-  | firstChar == char = 1 + countAndRemove chars char
+import Data.List ( mapAccumL )
 
-charCount :: [a] -> ()
+-- The first character, number of occurences of the first character in a given string, and the leftover characters after all occurences of the first character are removed
+-- Example: countFirstAndRemove "abbac" = ('a', 2, "bbc")
+countFirstAndRemove string = (firstChar, occurences, concat leftover)
+  where firstChar              = head string
+        (occurences, leftover) = mapAccumL (\a b -> (a + fromEnum (b == firstChar), if b /= firstChar then [b] else [])) 0 string
+
+-- A list of each unique char in a given string and the number of times it occures
+-- Example: countAll "abbac" = [('a',2),('b',2),('c',1)]
+countAll string
+  | null string = []
+  | otherwise   = (char, occurences) : countAll leftover
+    where (char, occurences, leftover) = countFirstAndRemove string
+
 charCount string = ()
-  where firstChar = head string
-        filteredString = filter (/= firstChar) string
-        firstCharCount = count 
+  -- where firstChar = head string
+  --       filteredString = filter (/= firstChar) string
+  --       firstCharCount = count
 
 main :: IO ()
 main = do
   string <- getLine
-  print string
+  print $ countAll string
   return ()
