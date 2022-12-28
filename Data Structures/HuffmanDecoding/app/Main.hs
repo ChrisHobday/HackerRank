3f7cc5ec7a6907b2eae5070f024d57f371a0504a
ChrisHobday <hobday.chris@protonmail.com>
2022-12-27 23:03:15 -0500

    Update HuffmanDecoding challenge, add tree data model, sortCharFrequencies, and constructTree

--- Data Structures/HuffmanDecoding/app/Main.hs
+++ Data Structures/HuffmanDecoding/app/Main.hs
@@ -1,24 +1,37 @@
 module Main (main) where
 
-import Data.List ( mapAccumL )
+import Data.List ( mapAccumL
+                 , sortBy )
+-- import Data.
+
+-- A data model representing a binary tree
+data Tree a = Leaf
+            | Node a (Tree a) (Tree a)
+  deriving ( Show )
 
 -- The first character, number of occurences of the first character in a given string, and the leftover characters after all occurences of the first character are removed
 -- Example: countFirstAndRemove "abbac" = ('a', 2, "bbc")
+countFirstAndRemove :: Eq a => [a] -> (a, Int, [a])
 countFirstAndRemove string = (firstChar, occurences, concat leftover)
   where firstChar              = head string
         (occurences, leftover) = mapAccumL (\a b -> (a + fromEnum (b == firstChar), if b /= firstChar then [b] else [])) 0 string
 
 -- A list of each unique char in a given string and the number of times it occures
 -- Example: countAll "abbac" = [('a',2),('b',2),('c',1)]
+countAll :: Eq a => [a] -> [(a, Int)]
 countAll string
   | null string = []
   | otherwise   = (char, occurences) : countAll leftover
     where (char, occurences, leftover) = countFirstAndRemove string
 
-charCount string = ()
-  -- where firstChar = head string
-  --       filteredString = filter (/= firstChar) string
-  --       firstCharCount = count
+-- A list of character frequencies sorted by least to most frequent
+-- Example: sortCharFrequencies [('a',2),('b',2),('c',1)] = [('c',1),('a',2),('b',2)]
+sortCharFrequencies charFrequencies = sortBy (\(_,a) (_,b) -> compare a b) charFrequencies
+
+-- constructTree :: [(Char, Int)] -> Tree (Maybe Char, Int)
+constructTree ((char, occurences) : (char', occurences') : charFrequencies) = Node (Nothing, combinedOccurences) (Node (Just char, occurences) Leaf Leaf) (Node (Just char', occurences') Leaf Leaf)
+  where combinedOccurences = occurences + occurences'
+
 
 main :: IO ()
 main = do
