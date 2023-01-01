8090a05cf45c8a904f06aee30dd936cc650d304a
ChrisHobday <hobday.chris@protonmail.com>
2022-12-31 22:17:37 -0500

    Update HuffmanDecoding challenge, complete constructTree

--- Data Structures/HuffmanDecoding/app/Main.hs
+++ Data Structures/HuffmanDecoding/app/Main.hs
@@ -30,19 +30,25 @@ countAll string
 -- Example: sortCharFrequencies [('a',2),('b',2),('c',1)] = [('c',1),('a',2),('b',2)]
 sortCharFrequencies charFrequencies = sortBy (\(_,a) (_,b) -> compare a b) charFrequencies
 
--- buildNode x = if 2*x + 1 > 7 then (x, []) else (x, [2*x, 2*x+1])
--- buildNode x = if 2*x + 1 > 7 then (x, []) else (x, [2*x, 2*x+1])
+-- A Huffman Decoding tree constructed from a given list of character, occurence pairs and maybe an existing tree
+-- Example: constructTree [('c',1),('a',2),('b',2)] Nothing = Node {rootLabel = (Nothing,5), subForest = [Node {rootLabel = (Just 'b',2), subForest = []},Node {rootLabel = (Nothing,3), subForest = [Node {rootLabel = (Just 'c',1), subForest = []},Node {rootLabel = (Just 'a',2), subForest = []}]}]}
+constructTree :: (Num b, Ord b) => [(a, b)] -> Maybe (Tree (Maybe a, b)) -> Tree (Maybe a, b)
+-- There is no character frequencies but is a tree
+constructTree [] (Just tree)                                                        = tree
+-- There is no character frequencies and no tree
+constructTree [] Nothing                                                            = Node (Nothing, 0) []
+-- There is at least one character frequency and a tree
+-- constructTree ((char, occurences) : charFrequencies) (Just tree)@(Node (_, occurences') _) = tree
+constructTree ((char, occurences) : charFrequencies) (Just tree@(Node (_, occurences') _))
+  -- Given tree should be on left
+  | occurences >= occurences' = constructTree charFrequencies (Just (Node (Nothing, occurences + occurences') [tree, Node (Just char, occurences) []]))
+  -- Given tree should be on left
+  | otherwise                 = constructTree charFrequencies (Just (Node (Nothing, occurences + occurences') [Node (Just char, occurences) [], tree]))
+-- There is at least two character frequencies but no tree
+constructTree ((char, occurences) : (char', occurences') : charFrequencies) Nothing = constructTree charFrequencies (Just (Node (Nothing, occurences + occurences') [Node (Just char, occurences) [], Node (Just char', occurences') []]))
+-- There is only one given character frequenct but no tree
+constructTree ((char, occurences) : charFrequencies) Nothing = Node (Just char, occurences) []
 
-eT :: Maybe (Either (Tree (Maybe Char, Int)) (Tree (Maybe Char, Int))) -> String
-eT tree = either (\t -> "Lefty loosy " ++ show t) (\t -> "Righty tighty " ++ show t) (fromJust tree)
-
--- constructTree :: [(Char, Int)] -> Maybe (Either (Tree (Maybe Char, Int)) (Tree (Maybe Char, Int))) -> Tree (Maybe Char, Int)
--- constructTree ((char, occurences) : (char', occurences') : charFrequencies) tree
---   | [] _      = tree
---   | otherwise = 
---   where LeftNode  = Node (Just char, occurences)
---         RightNode = Node (char', occurences')
---         RootNode  = Node (Nothing, occurences + occurences') [LeftNode, RightNode]
 
 -- constructTree :: [(Char, Int)] -> Tree (Maybe Char, Int)
 -- constructTree ((char, occurences) : (char', occurences') : charFrequencies) = constructTree ((Nothing, combinedOccurences) : charFrequencies) (Node (Just char, occurences) Empty Empty) (Node (Just char', occurences') Empty Empty)
