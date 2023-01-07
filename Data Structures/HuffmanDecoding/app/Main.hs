be68d0cd50b1701adcc99772a88a06bd682ce83b
ChrisHobday <hobday.chris@protonmail.com>
2023-01-07 00:28:45 -0500

    Complete HuffmanDecoding challenge, add encode function, and print line of input encoded then decoded

--- Data Structures/HuffmanDecoding/app/Main.hs
+++ Data Structures/HuffmanDecoding/app/Main.hs
@@ -1,13 +1,16 @@
 module Main (main) where
 
 import Data.List ( mapAccumL
-                 , sortBy )
-import Data.Tree ( Tree ( Node
-                        , rootLabel )
-                 , drawTree
-                 , flatten
-                 , foldTree )
+                 , sortBy
+                 , lookup )
+import Data.Tree ( Tree ( Node ) )
 import Data.Maybe ( fromJust )
+
+-- Note: This challenge is poorly worded and designed. By the description it's not clear what is expected input/output should be.
+-- In actuality the input is a string of characters and the output is supposed to be the same string of characters, so you could
+-- technically beat the challenge by simply outputing the string you get as input. In the spirit of the challenge, here we use the 
+-- inputted string to contruct a huffman tree, use that tree to construct a character encoding list, use that list to create an
+-- encoding of the original string, and then use that encoding and the tree to decode the original input.
 
 -- The first character, number of occurences of the first character in a given string, and the leftover characters after all occurences of the first character are removed
 -- Example: countFirstAndRemove "abbac" = ('a', 2, "bbc")
@@ -53,7 +56,7 @@ x = constructTree (sortCharFrequencies $ countAll "ABRACADABRA") Nothing
 -- Example: charEncodings (Node (Nothing, 1) [Node (Just 'b', 2) [], Node (Just 'c', 3) []]) = [('b',"0"),('c',"1")]
 charEncodings :: Tree (Maybe Char, b) -> [(Char, String)]
 charEncodings tree = convert "" tree
-  -- A help function which makes the inputs of charEncodings nicer
+  -- A helper function which makes the inputs of charEncodings nicer
   -- A list of characters and their binary encodings, built from a given starting encoding and tree
   -- Example: convert "" (Node (Nothing, 1) [Node (Just 'b', 2) [], Node (Just 'c', 3) []]) = [('b',"0"),('c',"1")]
   where convert :: String -> Tree (Maybe Char, b) -> [(Char, String)]
@@ -62,9 +65,37 @@ charEncodings tree = convert "" tree
         -- The tree only has a left child node
         convert code (Node (_, _) (leftNode : _))             = (convert (code ++ "0") leftNode )
         -- The tree has no children nodes
-        convert code (Node (char, _) [])                      = [(fromJust char, code)]
+        convert code (Node (Just char, _) [])                 = [(char, code)]
+        -- The tree has no children nodes and the root node is Nothing (This should not be possible in a huffman tree)
+        convert code (Node (Nothing, _) [])                   = undefined -- Error
+
+-- The huffman encoding of a given string
+encode :: String -> String
+encode string = concat $ encodeCharacter (charEncodings tree) <$> string 
+  where tree = constructTree (sortCharFrequencies (countAll string)) Nothing
+        -- The huffman encoding of a given character using a given list of character encodings
+        encodeCharacter encodings char = fromJust $ lookup char encodings
+
+-- The decoded string of a given code and tree
+-- Example: decode "010" (Node (Nothing, 1) [Node (Just 'b', 2) [], Node (Just 'c', 3) []]) = "bcb"
+decode :: String -> Tree (Maybe Char, b) -> String
+decode code tree = decode' code tree
+        -- A helper function so that the initial tree doesn't go out of scope when traversing it
+  where decode' :: String -> Tree (Maybe Char, b) -> String
+        -- There are no more bits to decode, and the tree has no child nodes (We've arrived at the last character)
+        decode' "" (Node (Just char, _) []) = [char]
+        -- There are more bits to decode, and the tree has no child nodes (We've arrived at a character)
+        decode' code (Node (Just char, _) [])  = char : decode code tree
+        -- 
+        decode' (bit : bits) (Node (_, _) (leftChild : rightChild : _))
+          -- The next bit in the code is 0
+          | bit == '0' = decode' bits leftChild -- Continue decoding character with left child
+          -- Otherwise (The next bit in the code is 1)
+          | otherwise  = decode' bits rightChild -- Continue decoding character with right child
+        -- Otherwise (This should not be possible in a huffman tree)
+        decode' _ _ = undefined
 
 main :: IO ()
 main = do
-  string <- getLine
-  putStrLn $ drawTree $ fmap show $ constructTree (sortCharFrequencies $ countAll string) Nothing
+  string <- getLine -- Read and bind string to encode and decode
+  putStrLn $ decode (encode string) (constructTree (sortCharFrequencies (countAll string)) Nothing) -- Print given string encoded and decoded
