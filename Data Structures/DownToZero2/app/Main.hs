be11d1f78a83272d2a1bba2c6176e0133d48210f
ChrisHobday <hobday.chris@protonmail.com>
2023-01-09 22:10:56 -0500

    Update DownToZero2 challenge, add skeleton code

--- Data Structures/DownToZero2/app/Main.hs
+++ Data Structures/DownToZero2/app/Main.hs
@@ -1,6 +1,10 @@
 module Main (main) where
 
-import Lib
+import Control.Monad ( replicateM )
 
 main :: IO ()
-main = someFunc
+main = do
+  numberOfQueries <- readLn :: IO Int -- Read and bind number of queries to be entered
+  queries <- replicateM numberOfQueries $ do -- Replicate the following action for each query
+    readLn :: IO Int -- Read query
+  print queries
