ebf15cee1bb517c6721b02a62868e7851e42175f
ChrisHobday <hobday.chris@protonmail.com>
2023-01-12 07:09:12 -0500

    Update DownToZero2 challenge, optimize for tailcall recursion

--- Data Structures/DownToZero2/app/Main.hs
+++ Data Structures/DownToZero2/app/Main.hs
@@ -35,72 +35,17 @@ factors n = factors' 1
 isPrime :: Integral a => a -> Bool
 isPrime a = a > 1 && null [ x | x <- [2 .. round $ sqrt $ fromIntegral a], a `mod` x == 0]
 
--- The smallest number of steps to get a number down to zero when you can only either 1) subtract the number by 1 or 2) use the max number of a factor pair
--- Example: downToZero2 5 = 4
--- downToZero2 :: Integral a => a -> Int
--- downToZero2 n = steps n
---   where -- The next step down to get a number down to zero
---         -- Example: stepDown 4 = 2
---         stepDown a
---           | a == 0              = 0
---           -- a is prime
---           | isPrime a || a == 1 = a - 1
---           -- a has factors we can use to reduce it
---           | otherwise           = if length minusStep <= length primeStep
---                                     then a - 1
---                                     else smallestMaxFactor
---                                     where -- The factors of a
---                                           aFactors          = factors a
---                                           -- The smallest max number of all factor pairs of the number
---                                           smallestMaxFactor = aFactors !! (length aFactors `div` 2)
---                                           -- The steps to zero with minus 1 option chosen now
---                                           minusStep         = steps (a - 1)
---                                           -- The steps to zero with prime option chosen now
---                                           primeStep         = steps smallestMaxFactor
---         -- The smallest list of steps to get a number down to zero
---         steps b
---           | stepDown b == 0 = [0]
---           -- There are more steps
---           | otherwise       = nextStep : steps nextStep
---                                 where nextStep = stepDown b
-
--- precomputed n = precomputed' []
---   where 
---     precomputed' as
---       | n < lengthAs  = as
---       | null as       = precomputed' $ 0 : as
---       | lengthAs == 1 = precomputed' $ 1 : as
---       | lengthAs == 2 = precomputed' $ 2 : as
---       | lengthAs == 3 = precomputed' $ 3 : as
---       | otherwise     = if isPrime currentNumber
---                           then precomputed' $ minusStep : as
---                           else precomputed' $ minSteps : as
---       where -- The current number we are calculating the number of steps for
---             currentNumber = lengthAs
---             -- The factors of the current number
---             currentNumberFactors = factors currentNumber
---             -- The smallest max number of all factor pairs of the current number
---             smallestMaxFactor = currentNumberFactors !! (length currentNumberFactors `div` 2)
---             -- The length of as
---             lengthAs = length as
---             -- The number of steps to zero with minus 1 option chosen now
---             minusStep = head as + 1
---             -- The number of steps to zero with prime option chosen now
---             primeStep = as !! (lengthAs - smallestMaxFactor - 1) + 1
---             -- The minimum number of steps to zero for the current number
---             minSteps  = min minusStep primeStep
-
 precomputed n = V.head $ precomputed' V.empty
   where precomputed' :: V.Vector Int -> V.Vector Int
         precomputed' as
           | n < lengthAs  = as
-          | V.null as     = precomputed' $ V.singleton 0 <> as
-          | lengthAs == 1 = precomputed' $ V.singleton 1 <> as
-          | lengthAs == 2 = precomputed' $ V.singleton 2 <> as
-          | lengthAs == 3 = precomputed' $ V.singleton 3 <> as
+          | V.null as     = as <> precomputed' (V.singleton 0)
+          | lengthAs == 1 = as <> precomputed' (V.singleton 1)
+          | lengthAs == 2 = as <> precomputed' (V.singleton 2)
+          | lengthAs == 3 = as <> precomputed' (V.singleton 3)
           | otherwise     = if isPrime currentNumber
-                              then precomputed' $ V.singleton minusStep <> as
-                              else precomputed' $ V.singleton minSteps <> as
+                              then precomputed' $ as <> V.singleton minusStep
+                              else precomputed' $ as <> V.singleton minSteps
           where -- The length of as
                 lengthAs = V.length as
                 -- The current number we are calculating the number of steps for
@@ -110,11 +55,11 @@ precomputed n = V.head $ precomputed' V.empty
                 -- The smallest max number of all factor pairs of the current number
                 smallestMaxFactor = currentNumberFactors V.! (length currentNumberFactors `div` 2)
                 -- The number of steps to zero with minus 1 option chosen now
-                minusStep = V.head as + 1
+                minusStep = V.last as + 1
                 -- The number of steps to zero with prime option chosen now
-                primeStep = as V.! (lengthAs - smallestMaxFactor - 1) + 1
+                primeStep = as V.! (smallestMaxFactor + 1) + 1
                 -- The minimum number of steps to zero for the current number
-                minSteps  = min minusStep primeStep                         
+                minSteps  = min minusStep primeStep    
 
 main :: IO ()
 main = do
