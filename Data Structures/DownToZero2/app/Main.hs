35560298813d13ad5939c315cd07988d0d52b039
ChrisHobday <hobday.chris@protonmail.com>
2023-01-16 18:07:52 -0500

    Finish DownToZero2 challenge, clean up comments

--- Data Structures/DownToZero2/app/Main.hs
+++ Data Structures/DownToZero2/app/Main.hs
@@ -7,14 +7,16 @@ import qualified Data.Vector as V
 import qualified Data.Vector.Mutable as MV
 
 -- Note: The trick to this problem, once you wrap your head around what it's asking you to do in the problem statement.
--- 1. Realise
+-- 1. Realise the use of factors/prime numbers
+-- 2. Prebuild entire list of smallest number of steps to zero for all values 1-1000001 starting at 1
+-- 3. Use mutable vectors for speed (Lists and even mutable vectors were not fast enough for hackerrank time limit)
 
 mutablePrecomputed n = do
-  let intMax = maxBound :: Int
-  mv <- MV.replicate (n + 1) intMax
-  -- mv <- MV.new (n + 1)
-  -- mv <- MV.generate (n + 1) id -- Create and bind mutable vector of n + 1 size, each index filled with it's own value (Example: MV.generate 10 id = [0,1,2,3,4,5,6,7,8,9])
+  let maxInt = maxBound :: Int -- Maximum int size
 
+  mv <- MV.replicate (n + 1) maxInt -- Create and bind mutable vector of size n + 1 filled with max int
+
+  -- Write 0,1,2 as first 3 values of mutable vector
   MV.write mv 0 0
   MV.write mv 1 1
   MV.write mv 2 2
@@ -23,15 +25,15 @@ mutablePrecomputed n = do
     currentValue  <- MV.read mv i -- Bind current value at index i
     previousValue <- MV.read mv (i - 1) -- Bind previous value at index i - 1
     MV.write mv i (min currentValue (previousValue + 1)) -- Write min of current value or previous value + 1 to mutable vector index i
-    let nDivValue = min (n `div` i) i
-    forM_ [2 .. nDivValue] (\j -> do -- For each number from 2 to 
+    let nDivValue = min i (n `div` i) -- The number of possible upcoming vector positions this position could be jumped to from
+    forM_ [2 .. nDivValue] (\j -> do -- For each number from 2 to n div value
       currentValue2 <- MV.read mv (i * j) -- Bind current value 2 at index i * j
       factorValue   <- MV.read mv i -- Bind factor value at index i
       MV.write mv (i * j) (min currentValue2 (factorValue + 1)) -- Write min of current value 2 or factor value + 1 to mutable vectors index i * j
       )
     )
 
-  V.freeze mv
+  V.freeze mv -- Convert mutable vector into immutable vector to be used outside this monad
 
 main :: IO ()
 main = do
@@ -39,6 +41,6 @@ main = do
   queries <- replicateM numberOfQueries $ do -- Replicate the following action for each query
     readLn :: IO Int -- Read query
 
-  v <- mutablePrecomputed (maximum queries) -- Create and bind a vector of smallest number of steps to zero for each number from 1 to the maximum queried
+  v <- mutablePrecomputed 1000001 -- Create and bind a vector of smallest number of steps to zero for each number from 1 to 1000001
 
-  mapM_ (print . (v V.!)) queries
+  mapM_ (print . (v V.!)) queries -- Print the value at 
