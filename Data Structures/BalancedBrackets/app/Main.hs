f5e82bde0984ecc720f454c40ce9d7fe918b24c7
ChrisHobday <hobday.chris@protonmail.com>
2023-01-17 09:38:22 -0500

    Update BalancedBrackets challenge, add skeleton code

--- Data Structures/BalancedBrackets/app/Main.hs
+++ Data Structures/BalancedBrackets/app/Main.hs
@@ -1,4 +1,14 @@
 module Main (main) where
 
+import Control.Monad ( replicateM
+                     , mapM_ )
+
+isBalanced bracketString = bracketString
+
 main :: IO ()
-main = undefined
+main = do
+  numberOfStrings <- (readLn :: IO Int) -- Read and bind number of strings to be entered
+  bracketStrings <- replicateM numberOfStrings $ do -- Replicate the following action for each string to be entered
+    getLine -- Read string
+
+  mapM_ (print . isBalanced) bracketStrings -- Print wheter each bracket string is balanced or not
