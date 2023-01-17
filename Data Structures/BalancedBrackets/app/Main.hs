5eabbc1cb3dfefc1ea107c388b6e4ec449205175
ChrisHobday <hobday.chris@protonmail.com>
2023-01-17 15:19:06 -0500

    Finish DownToZero2 challenge, refactor isBalanced function

--- Data Structures/BalancedBrackets/app/Main.hs
+++ Data Structures/BalancedBrackets/app/Main.hs
@@ -1,7 +1,6 @@
 module Main (main) where
 
-import Control.Monad ( replicateM
-                     , mapM_ )
+import Control.Monad ( replicateM )
 
 -- Whether two given brackets match or not
 -- Example: matchingBrackets '(' ']' = False
@@ -11,12 +10,36 @@ matchingBrackets '{' '}' = True
 matchingBrackets '[' ']' = True
 matchingBrackets _ _     = False
 
-isBalanced :: [a] -> ([a], [a])
-isBalanced bracketString = (firstHalf, secondHalf)
-  where (firstHalf, secondHalf) = splitAt (length bracketString `div` 2) bracketString
-        go bracket bracket'
-          | bracket == bracket' = True
-          | otherwise           = False
+-- Whether two given brackets allow a bracket string to continue
+-- Example: continuingBrackets '(' '{' = True
+continuingBrackets :: Char -> Char -> Bool
+continuingBrackets '(' '(' = True
+continuingBrackets '(' '[' = True
+continuingBrackets '(' '{' = True
+continuingBrackets '{' '(' = True
+continuingBrackets '{' '[' = True
+continuingBrackets '{' '{' = True
+continuingBrackets '[' '(' = True
+continuingBrackets '[' '[' = True
+continuingBrackets '[' '{' = True
+continuingBrackets _ _     = False
+
+-- Whether a given bracket string is balanced or not
+-- Example: isBalanced "{{[()]}}" = "YES"
+-- isBalanced :: String -> String
+isBalanced bracketString
+  | go [] bracketString = "YES"
+  | otherwise           = "NO"
+  where -- Compares latest unmatched bracket with next bracket in bracket string
+        go :: [Char] -> [Char] -> Bool
+        go (x : xs) (y : ys)
+          | matchingBrackets x y   = go xs ys
+          | continuingBrackets x y = go (y : x : xs) ys
+          | otherwise              = False
+        go [] (y : ys) = go [y] ys
+        go (x : xs) [] = False
+        go [] []       = True
+
 
 main :: IO ()
 main = do
@@ -24,4 +47,4 @@ main = do
   bracketStrings <- replicateM numberOfStrings $ do -- Replicate the following action for each string to be entered
     getLine -- Read string
 
-  mapM_ (print . isBalanced) bracketStrings -- Print wheter each bracket string is balanced or not
+  mapM_ (putStrLn . isBalanced) bracketStrings -- Print wheter each bracket string is balanced or not
