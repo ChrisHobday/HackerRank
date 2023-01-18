61f8a361b6faed48c0983c4292a2a0d770ddbca9
ChrisHobday <hobday.chris@protonmail.com>
2023-01-18 18:14:27 -0500

    Update QueueUsingTwoStacks challenge, switch FIFO queue implementation to use Seq instead of lists

--- Data Structures/QueueUsingTwoStacks/app/Main.hs
+++ Data Structures/QueueUsingTwoStacks/app/Main.hs
@@ -1,20 +1,21 @@
 module Main (main) where
 
 import Control.Monad ( replicateM )
+import qualified Data.Sequence as S
 
--- Note: This FIFO queue is simulated with linked lists
+-- Note: This FIFO queue is simulated with a Seq (finger tree from Data.Sequence, which allows fast access to both front and back)
 
 -- A queue with a new value added to the back
-enqueue :: a -> [a] -> [a]
-enqueue value queue = queue ++ [value]
+enqueue :: a -> S.Seq a -> S.Seq a
+enqueue value queue = queue S.|> value
 
 -- A queue with it's front element removed
-dequeue :: [a] -> [a]
-dequeue (value : values) = values
-dequeue []               = []
+dequeue :: S.Seq a -> S.Seq a
+dequeue (value S.:<| values) = values
+dequeue S.Empty              = S.empty
 
 -- The evaluation of a list of queries and queue
-evaluateQueries :: (Eq a, Num a, Show a) => [[a]] -> [a] -> IO ()
+evaluateQueries :: (Eq a, Num a, Show a) => [[a]] -> S.Seq a -> IO ()
 -- The query type is 1
 evaluateQueries ((1 : queryValue : _) : queries) queue = evaluateQueries queries $ enqueue queryValue queue
 evaluateQueries ((queryType : _) : queries) queue
@@ -22,12 +23,11 @@ evaluateQueries ((queryType : _) : queries) queue
   | queryType == 2 = evaluateQueries queries $ dequeue queue
   -- The query type is 3
   | queryType == 3 = do
-                       print $ head queue
+                       let (frontValue S.:< _) = S.viewl queue -- The front element of the queue
+                       print frontValue
                        evaluateQueries queries queue
 -- There is not a known query type next in the list of given queries
 evaluateQueries _ _ = return ()
-
-testQueries = [[1,42],[2],[1,14],[3],[1,28],[3],[1,60],[1,78],[2],[2]]
 
 main :: IO ()
 main = do
@@ -35,4 +35,4 @@ main = do
   queries <- replicateM numberOfQueries $ do -- Replicate the following action for each query to be entered
     (read <$>) . words <$> getLine :: IO [Int] -- Read and bind query type and value
 
-  evaluateQueries queries [] -- Evaluate queries with empty starting queue
+  evaluateQueries queries S.empty -- Evaluate queries with empty starting queue
