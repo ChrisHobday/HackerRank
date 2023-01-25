83bc749ff4b1bea65d722c5f428eb3523d371775
ChrisHobday <hobday.chris@protonmail.com>
2023-01-25 16:26:10 -0500

    Finish SimpleTextEditor challenge, remove append, delete, print', undo functions, add evaluateOperation, evaluateOperations

--- Data Structures/SimpleTextEditor/app/Main.hs
+++ Data Structures/SimpleTextEditor/app/Main.hs
@@ -1,26 +1,46 @@
 module Main (main) where
 
 import Control.Monad ( replicateM )
+import Data.Maybe ( fromJust )
 
-append :: String -> String -> String
-append string string' = undefined
+-- Evaluate a given operation with a given list of strings, returning the new list of strings
+-- Example: ["abc",""] <- evaluateOperation (1, Just "abc") [""]
+evaluateOperation :: (Eq a, Num a) => (a, Maybe String) -> [String] -> IO [String]
+evaluateOperation (operationType, argument) (string : strings)
+  -- Operation is append characters
+  | operationType == 1 = do
+                           return (appendedString : string : strings) -- Return list of strings with new appended string in front
+  -- Operation is delete an amount of characters
+  | operationType == 2 = do
+                           return (deletedString : string : strings) -- Return list of strings with new deleted string in front
+  -- Operation is print a character
+  | operationType == 3 = do
+                           putStrLn [(string !! characterToPrint)] -- Print proper character
+                           return (string : strings) -- Return list of strings
+  -- Operation is undo
+  | operationType == 4 = do
+                           return strings -- Return list of strings with previous string removed
+  where -- The previous string with characters appended to the end
+        appendedString = string ++ fromJust argument
+        -- The number of characters to delete from the end of previous string (only evaluated if operation type is 2)
+        numberOfCharactersToDelete = read $ fromJust argument :: Int
+        -- The previous string with characters deleted from the end
+        deletedString = take (length string - numberOfCharactersToDelete) string
+        -- Character to print
+        characterToPrint = (read $ fromJust argument) - 1 :: Int
 
-delete :: Int -> String -> String
-delete numberOfChars string = undefined
-
-print' :: String -> IO ()
-print' string = undefined
-
-undo :: [String] -> (String, [String])
-undo previousStringStates = undefined
-
-evaluateOperations :: [(Int, Maybe String)] -> IO ()
-evaluateOperations operations = undefined
+-- Evaluate a list of operations on a list of strings
+-- Example: evaluateOperations [(1, Just "abc"), (3, Just "2")] [""] = prints b to the console
+evaluateOperations :: (Eq a, Num a) => [(a, Maybe String)] -> [String] -> IO ()
+evaluateOperations [] _ = do return ()
+evaluateOperations (operation : restOfoperations) strings = do
+                                                newStrings <- evaluateOperation operation strings -- Evaluate operation, binding resulting new list of strings
+                                                evaluateOperations restOfoperations newStrings -- Evaluate rest of operations using new list of strings
 
 main :: IO ()
 main = do
   numberOfOperations <- (readLn :: IO Int) -- Read and bind number of operations to be entered
-  operations <- replicateM numberOfOperations $ do
+  operations <- replicateM numberOfOperations $ do -- For each operation to be entered...
     (operationType : argument) <- words <$> getLine -- Read and bind operation type and potential argument
     let operationType' = read operationType :: Int -- Operation type as an integer
     let argument'      = head argument -- Potential argument (not evaluated unless later needed)
@@ -30,6 +50,5 @@ main = do
       -- Operation has an argument
       _ -> return (operationType', Just argument')
   
-  evaluateOperations operations
-  
+  evaluateOperations operations [""] -- Evaluate the entered operations with an empty starting string
   
