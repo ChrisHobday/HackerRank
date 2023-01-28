efe91c2635bd279972d6283be36ac447dc46aba8
ChrisHobday <hobday.chris@protonmail.com>
2023-01-27 20:48:15 -0500

    Update SwapNodesAlgo challenge, remove readTree, add buildTree

--- Data Structures/SwapNodesAlgo/app/Main.hs
+++ Data Structures/SwapNodesAlgo/app/Main.hs
@@ -1,11 +1,17 @@
 module Main (main) where
 
-import Control.Monad ( replicateM_ )
+import Control.Monad ( replicateM )
 import Data.Tree
 
--- Read a binary tree with a given amount of nodes and return it
-readTree numberOfNodes = do
-  return (Node 1 [])
+-- buildTree node node
+
+-- -- Read a binary tree with a given amount of nodes and return it
+-- readTree numberOfNodes = do
+
+--   return (Node 1 [])
+
+buildTree :: [[Int]] -> Tree Int
+buildTree nodes = Node 1 []
 
 -- A list representing the in order traversal of nodes of a given binary tree
 -- Example: (Node 1 [Node 2 [], Node 3 []]) = [2, 1, 3]
@@ -17,9 +23,15 @@ swapNodes depth tree = tree
 main :: IO ()
 main = do
   numberOfNodes <- readLn :: IO Int -- Read and bind number of nodes to be entered
-  binaryTree <- readTree numberOfNodes -- Read and bind binary tree
-  numberOfQueries <- readLn :: IO Int -- Read and bind number of queries to be entered
-  queries <- replicateM_ numberOfQueries $ do -- For each query to be entered...
-    readLn :: IO Int -- Read query
+  nodes <- replicateM numberOfNodes $ do -- For each node to be entered...
+    (read <$>) . words <$> getLine :: IO [Int] -- Read nodes
 
-  print "SwapNodesAlgo"
+  let binaryTree = buildTree nodes
+
+  print binaryTree
+
+  -- numberOfQueries <- readLn :: IO Int -- Read and bind number of queries to be entered
+  -- queries <- replicateM numberOfQueries $ do -- For each query to be entered...
+  --   readLn :: IO Int -- Read query
+
+  -- print queries
