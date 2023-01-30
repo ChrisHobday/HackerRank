6005dcf10e336c58afa0956bbc34a32cd6af8b21
ChrisHobday <hobday.chris@protonmail.com>
2023-01-30 15:18:09 -0500

    Update SwapNodesAlgo challenge, work on building a tree from a list of sibling nodes

--- Data Structures/SwapNodesAlgo/app/Main.hs
+++ Data Structures/SwapNodesAlgo/app/Main.hs
@@ -3,15 +3,21 @@ module Main (main) where
 import Control.Monad ( replicateM )
 import Data.Tree
 
--- buildTree node node
+buildNode value
+  | value == -1 = Nothing
+  | otherwise   = Just $ Node value []
 
--- -- Read a binary tree with a given amount of nodes and return it
--- readTree numberOfNodes = do
+buildSiblings :: (Eq a, Num a) => [a] -> Int -> [[Int]] -> [Tree a]
+buildSiblings [] _ _                        = []
+buildSiblings (firstSibling : restSiblings) numberOfCousins siblingPairs
+  | firstSibling == (-1) = buildSiblings restSiblings (numberOfCousins -1) siblingPairs
+  | otherwise            = Node firstSibling [] : buildSiblings restSiblings (numberOfCousins -1) siblingPairs
 
---   return (Node 1 [])
-
-buildTree :: [[Int]] -> Tree Int
-buildTree nodes = Node 1 []
+-- buildTree :: [[Int]] -> Int -> Tree Int
+buildTree nodes@(firstSiblingPair : restOfSiblingPairs) numberOfNodesToBuild = buildSiblings firstSiblingPair
+  where nextNumberOfNodesToBuild = length $ filter (/= -1) $ concat $ take numberOfNodesToBuild nodes
+buildTree _ _ = undefined
+        
 
 -- A list representing the in order traversal of nodes of a given binary tree
 -- Example: (Node 1 [Node 2 [], Node 3 []]) = [2, 1, 3]
@@ -20,18 +26,22 @@ inOrderTraversal tree = []
 -- Swap the nodes of a given tree at a given depth
 swapNodes depth tree = tree
 
+testNodeList = [[2, 3], [4, -1], [5, -1], [6, -1], [7, 8], [-1, 9], [-1, -1], [10, 11], [-1, -1], [-1, -1], [-1, -1]]
+
 main :: IO ()
 main = do
   numberOfNodes <- readLn :: IO Int -- Read and bind number of nodes to be entered
   nodes <- replicateM numberOfNodes $ do -- For each node to be entered...
     (read <$>) . words <$> getLine :: IO [Int] -- Read nodes
 
-  let binaryTree = buildTree nodes
+  -- let binaryTree = buildTree nodes
 
-  print binaryTree
+  -- print binaryTree
 
   -- numberOfQueries <- readLn :: IO Int -- Read and bind number of queries to be entered
   -- queries <- replicateM numberOfQueries $ do -- For each query to be entered...
   --   readLn :: IO Int -- Read query
 
   -- print queries
+
+  return ()
