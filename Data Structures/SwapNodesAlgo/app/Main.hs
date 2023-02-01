11e4b78abb9e972c6eec26914cc2c15b17d8bf1a
ChrisHobday <hobday.chris@protonmail.com>
2023-01-31 20:16:25 -0500

    Update SwapNodesAlgo challenge, work on building tree

--- Data Structures/SwapNodesAlgo/app/Main.hs
+++ Data Structures/SwapNodesAlgo/app/Main.hs
@@ -1,23 +1,46 @@
 module Main (main) where
 
 import Control.Monad ( replicateM )
-import Data.Tree
+import Data.Tree ( Tree( Node ) )
 
 buildNode value
   | value == -1 = Nothing
   | otherwise   = Just $ Node value []
 
-buildSiblings :: (Eq a, Num a) => [a] -> Int -> [[Int]] -> [Tree a]
-buildSiblings [] _ _                        = []
-buildSiblings (firstSibling : restSiblings) numberOfCousins siblingPairs
-  | firstSibling == (-1) = buildSiblings restSiblings (numberOfCousins -1) siblingPairs
-  | otherwise            = Node firstSibling [] : buildSiblings restSiblings (numberOfCousins -1) siblingPairs
+buildSiblingNodes :: (Eq a, Num a) => [a] -> [Tree a]
+buildSiblingNodes [] = []
+buildSiblingNodes (firstSibling : restOfSiblings)
+  | firstSibling == -1 = buildSiblingNodes restOfSiblings
+  | otherwise          = Node firstSibling [] : buildSiblingNodes restOfSiblings
+
+simplifyNodes [] = []
+simplifyNodes (Just node : restOfNodes) = node : simplifyNodes restOfNodes
+simplifyNodes (Nothing : restOfNodes)   = simplifyNodes restOfNodes
+
+-- buildTree nodes = 
+--   where siblingNodes   = (buildNode <$>) <$> nodes
+--         simpleNodeList = Node 1 [] : (filter (=/ Nothing) $ concat siblingNodes)
+
+-- buildSiblings :: (Eq a, Num a) => [a] -> Int -> [[Int]] -> [Tree a]
+-- buildSiblings [] _ _                        = []
+-- buildSiblings (firstSibling : restSiblings) numberOfCousins siblingPairs
+--   | firstSibling == (-1) = buildSiblings restSiblings (numberOfCousins -1) siblingPairs
+--   | otherwise            = Node firstSibling [] : buildSiblings restSiblings (numberOfCousins -1) siblingPairs
 
 -- buildTree :: [[Int]] -> Int -> Tree Int
-buildTree nodes@(firstSiblingPair : restOfSiblingPairs) numberOfNodesToBuild = buildSiblings firstSiblingPair
-  where nextNumberOfNodesToBuild = length $ filter (/= -1) $ concat $ take numberOfNodesToBuild nodes
-buildTree _ _ = undefined
-        
+-- buildTree nodes@(firstSiblingPair : restOfSiblingPairs) numberOfNodesToBuild = buildSiblings firstSiblingPair
+--   where nextNumberOfNodesToBuild = length $ filter (/= -1) $ concat $ take numberOfNodesToBuild nodes
+-- buildTree _ _ = undefined
+
+-- buildTree ((-1, -1) : restOfSiblingPairs) cousinNodesBefore cousinPairsAfter                      = []
+-- buildTree ((-1, secondSibling) : restOfSiblingPairs) cousinNodesBefore cousinPairsAfter           = [Node secondSibling []]
+-- buildTree ((firstSibling, -1) : restOfSiblingPairs) cousinNodesBefore cousinPairsAfter            = [Node firstSibling [buildTree restOfSiblings cousinNodesBefore cousinPairsAfter]]
+-- buildTree ((firstSibling, secondSibling) : restOfSiblingPairs) cousinNodesBefore cousinPairsAfter = [Node firstSibling [], Node secondSibling []]
+-- buildTree _ _ _ = undefined
+
+-- buildTree [] _ _ = []
+-- buildTree ((firstSibling : secondSibling : _) : restOfSiblingPairs) cousinNodesBefore cousinPairsAfter = [Node firstSibling (buildTree (drop (cousinNodesBefore + cousinPairsAfter) restOfSiblingPairs) 0 0), Node secondSibling (buildTree (drop (cousinNodesBefore + cousinPairsAfter + 1) restOfSiblingPairs) 0 0)]
+-- buildTree _ _ _ = undefined
 
 -- A list representing the in order traversal of nodes of a given binary tree
 -- Example: (Node 1 [Node 2 [], Node 3 []]) = [2, 1, 3]
@@ -27,6 +50,10 @@ inOrderTraversal tree = []
 swapNodes depth tree = tree
 
 testNodeList = [[2, 3], [4, -1], [5, -1], [6, -1], [7, 8], [-1, 9], [-1, -1], [10, 11], [-1, -1], [-1, -1], [-1, -1]]
+testNodeList2 = [[2, 3], [-1, -1], [-1, -1]]
+
+siblingNodes = buildSiblingNodes <$> testNodeList
+simpleNodeList = Node 1 [] :  concat siblingNodes
 
 main :: IO ()
 main = do
