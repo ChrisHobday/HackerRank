bb493bb7405dc6194ef2ebc9473054a2e4d17d38
ChrisHobday <hobday.chris@protonmail.com>
2023-02-17 14:40:17 -0500

    Update SwapNodesAlgo challenge, start rewriting buildTree functions with goal of simplifying

--- Data Structures/SwapNodesAlgo/app/Main.hs
+++ Data Structures/SwapNodesAlgo/app/Main.hs
@@ -3,56 +3,68 @@ module Main (main) where
 import Control.Monad ( replicateM )
 import Data.Tree
 
--- Build sibling nodes from a list of number pairs
--- Example: buildSiblingNodes [2, -1] = [Node {rootLabel = 2, subForest = []}]
-buildSiblingNodes :: (Eq a, Num a) => [a] -> [Tree a]
-buildSiblingNodes [] = []
-buildSiblingNodes (firstSibling : restOfSiblings)
-  -- | firstSibling == -1 = buildSiblingNodes restOfSiblings
-  | otherwise          = Node firstSibling [] : buildSiblingNodes restOfSiblings
+-- buildTree :: [Tree Int] -> [Tree Int]
+buildTree :: (Eq a, Num a) => [Tree a] -> [Tree a]
+buildTree (firstSibling : secondSibling : potentialParent : restOfPotentialParents)
+  -- Cannot be parent because either it's not a proper node or it already has children
+  | rootLabel potentialParent == -1 || not (null (subForest potentialParent)) = buildTree $ potentialParent : buildTree (firstSibling : secondSibling : restOfPotentialParents)
+  -- Parent found
+  | otherwise                                                                 = potentialParent { subForest = [firstSibling, secondSibling] } : restOfPotentialParents
+buildTree finalTree = finalTree
 
--- Group a list of of child nodes by their depth in the tree
--- Example: groupChildrenByDepth 1 [[Node {rootLabel = 1, subForest = []}],[Node {rootLabel = 2, subForest = []},Node {rootLabel = 3, subForest = []}]] = 
---   [[[Node {rootLabel = 1, subForest = []}]],[[Node {rootLabel = 2, subForest = []},Node {rootLabel = 3, subForest = []}]],[]]
-groupChildrenByDepth 0 _ = []
-groupChildrenByDepth numberOfChildrenNodePairsAtDepth childrenNodePairs = groupedChildren : groupChildrenByDepth numberOfChildrenToGroupNext restOfChildNodePairs
-  where (groupedChildren, restOfChildNodePairs) = splitAt numberOfChildrenNodePairsAtDepth childrenNodePairs
-        numberOfChildrenToGroupNext             = sum $ length <$> (filter ((/= -1) . rootLabel) <$> groupedChildren)
+siblingPairs = [[2, 3], [-1, 4], [-1, 5], [-1, -1], [-1, -1]]
+reversedNodeList = (\a -> Node a []) <$> concat (reverse $ [1] : siblingPairs)
 
--- Insert child pairs into their parent nodes
--- Example: insertChildPairs [[Node {rootLabel = 2, subForest = []},Node {rootLabel = 3, subForest = []}]] [[Node {rootLabel = 1, subForest = []}]] =
---   [[Node {rootLabel = 1, subForest = [Node {rootLabel = 2, subForest = []},Node {rootLabel = 3, subForest = []}]}]]
--- insertChildPairs :: [[Tree a]] -> [[Tree a]] -> [[Tree a]]
-insertChildPairs [] _ = []
-insertChildPairs _ [] = []
-insertChildPairs childPairs (firstParentPair : restOfParentPairs)
-  | null firstParentPair = [] : insertChildPairs childPairs restOfParentPairs
-  | otherwise            = zipWith (\a b -> if rootLabel b /= -1 then b { subForest = a } else b) childPairsToUse firstParentPair : insertChildPairs restOfChildPairs restOfParentPairs
-  where (childPairsToUse, restOfChildPairs) = splitAt (length $ filter ((/= -1) . rootLabel) firstParentPair) childPairs
+-- -- Build sibling nodes from a list of number pairs
+-- -- Example: buildSiblingNodes [2, -1] = [Node {rootLabel = 2, subForest = []}]
+-- buildSiblingNodes :: (Eq a, Num a) => [a] -> [Tree a]
+-- buildSiblingNodes [] = []
+-- buildSiblingNodes (firstSibling : restOfSiblings)
+--   -- | firstSibling == -1 = buildSiblingNodes restOfSiblings
+--   | otherwise          = Node firstSibling [] : buildSiblingNodes restOfSiblings
 
--- Build a tree from a given list of numberPairs
--- Example: buildTree [[2, 3],[-1, -1],[-1, -1]] = Node {rootLabel = 1, subForest = [Node {rootLabel = 2, subForest = []},Node {rootLabel = 3, subForest = []}]}
-buildTree :: (Num a, Eq a) => [[a]] -> Tree a
-buildTree siblingNodeList = head $ concat $ foldl1 insertChildPairs $ reverse $ groupChildrenByDepth 1 $ [Node 1 []] : (buildSiblingNodes <$> siblingNodeList)
+-- -- Group a list of of child nodes by their depth in the tree
+-- -- Example: groupChildrenByDepth 1 [[Node {rootLabel = 1, subForest = []}],[Node {rootLabel = 2, subForest = []},Node {rootLabel = 3, subForest = []}]] = 
+-- --   [[[Node {rootLabel = 1, subForest = []}]],[[Node {rootLabel = 2, subForest = []},Node {rootLabel = 3, subForest = []}]],[]]
+-- groupChildrenByDepth 0 _ = []
+-- groupChildrenByDepth numberOfChildrenNodePairsAtDepth childrenNodePairs = groupedChildren : groupChildrenByDepth numberOfChildrenToGroupNext restOfChildNodePairs
+--   where (groupedChildren, restOfChildNodePairs) = splitAt numberOfChildrenNodePairsAtDepth childrenNodePairs
+--         numberOfChildrenToGroupNext             = sum $ length <$> (filter ((/= -1) . rootLabel) <$> groupedChildren)
 
--- A list representing the in order traversal of nodes of a given binary tree
--- Example: (Node 1 [Node 2 [], Node 3 []]) = [2, 1, 3]
-inOrderTraversal (Node id (firstChild : secondChild : _)) = inOrderTraversal firstChild <> show id <> inOrderTraversal secondChild
-inOrderTraversal (Node id (firstChild : _)) = inOrderTraversal firstChild <> show id
-inOrderTraversal (Node id []) = show id
+-- -- Insert child pairs into their parent nodes
+-- -- Example: insertChildPairs [[Node {rootLabel = 2, subForest = []},Node {rootLabel = 3, subForest = []}]] [[Node {rootLabel = 1, subForest = []}]] =
+-- --   [[Node {rootLabel = 1, subForest = [Node {rootLabel = 2, subForest = []},Node {rootLabel = 3, subForest = []}]}]]
+-- -- insertChildPairs :: [[Tree a]] -> [[Tree a]] -> [[Tree a]]
+-- insertChildPairs [] _ = []
+-- insertChildPairs _ [] = []
+-- insertChildPairs childPairs (firstParentPair : restOfParentPairs)
+--   | null firstParentPair = [] : insertChildPairs childPairs restOfParentPairs
+--   | otherwise            = zipWith (\a b -> if rootLabel b /= -1 then b { subForest = a } else b) childPairsToUse firstParentPair : insertChildPairs restOfChildPairs restOfParentPairs
+--   where (childPairsToUse, restOfChildPairs) = splitAt (length $ filter ((/= -1) . rootLabel) firstParentPair) childPairs
 
--- Swap the nodes of a given tree at a given depth
-swapNodes depth tree = tree
+-- -- Build a tree from a given list of numberPairs
+-- -- Example: buildTree [[2, 3],[-1, -1],[-1, -1]] = Node {rootLabel = 1, subForest = [Node {rootLabel = 2, subForest = []},Node {rootLabel = 3, subForest = []}]}
+-- buildTree :: (Num a, Eq a) => [[a]] -> Tree a
+-- buildTree siblingNodeList = head $ concat $ foldl1 insertChildPairs $ reverse $ groupChildrenByDepth 1 $ [Node 1 []] : (buildSiblingNodes <$> siblingNodeList)
+
+-- -- A list representing the in order traversal of nodes of a given binary tree
+-- -- Example: (Node 1 [Node 2 [], Node 3 []]) = [2, 1, 3]
+-- inOrderTraversal (Node id (firstChild : secondChild : _)) = inOrderTraversal firstChild <> show id <> inOrderTraversal secondChild
+-- inOrderTraversal (Node id (firstChild : _)) = inOrderTraversal firstChild <> show id
+-- inOrderTraversal (Node id []) = show id
+
+-- -- Swap the nodes of a given tree at a given depth
+-- swapNodes depth tree = tree
 
 main :: IO ()
 main = do
-  numberOfNodes <- readLn :: IO Int -- Read and bind number of nodes to be entered
-  nodes <- replicateM numberOfNodes $ do -- For each node to be entered...
-    (read <$>) . words <$> getLine :: IO [Int] -- Read nodes
+  -- numberOfNodes <- readLn :: IO Int -- Read and bind number of nodes to be entered
+  -- nodes <- replicateM numberOfNodes $ do -- For each node to be entered...
+  --   (read <$>) . words <$> getLine :: IO [Int] -- Read nodes
 
-  let binaryTree = buildTree nodes
+  -- let binaryTree = buildTree nodes
 
-  putStrLn $ drawTree $ show <$> binaryTree
+  -- putStrLn $ drawTree $ show <$> binaryTree
 
   -- print $ inOrderTraversal binaryTree
 
