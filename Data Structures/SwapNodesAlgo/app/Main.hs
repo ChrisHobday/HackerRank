6d9c3e380c6b2741678cd6973acdf3e081132b3d
ChrisHobday <hobday.chris@protonmail.com>
2023-02-19 00:34:41 -0500

    Update SwapNodesAlgo challenge, start leftToRightTraversal function, continue inOrderTraversal function

--- Data Structures/SwapNodesAlgo/app/Main.hs
+++ Data Structures/SwapNodesAlgo/app/Main.hs
@@ -2,7 +2,14 @@ module Main (main) where
 
 import Control.Monad ( replicateM )
 import Data.Tree
+import Data.List ( intersperse )
 import Debug.Trace
+
+-- A list of tree nodes in reverse order from a given list of sibling pairs without a root node
+-- Example: reversedNodeList [[2, 3], [-1, -1], [-1, -1]] =
+-- [Node {rootLabel = -1, subForest = []},Node {rootLabel = -1, subForest = []},Node {rootLabel = -1, subForest = []},Node {rootLabel = -1, subForest = []},Node {rootLabel = 3, subForest = []},Node {rootLabel = 2, subForest = []},Node {rootLabel = 1, subForest = []}]
+reversedNodeList :: Num a => [[a]] -> [Tree a]
+reversedNodeList siblingPairs = (`Node` []) <$> reverse (concat $ [1] : siblingPairs)
 
 -- A tree build from a given list of nodes (lowest depth first)
 -- Example: buildTree [Node {rootLabel = -1, subForest = []},Node {rootLabel = -1, subForest = []},Node {rootLabel = -1, subForest = []},Node {rootLabel = -1, subForest = []},Node {rootLabel = 3, subForest = []},Node {rootLabel = 2, subForest = []},Node {rootLabel = 1, subForest = []}] =
@@ -18,7 +25,7 @@ buildTree nodes@(_ : _ : _ : _) = buildTree $ insertSiblings nodes
     -- Cannot be parent because either it's not a proper node or it already has children
       | rootLabel potentialParent == -1 || not (null (subForest potentialParent)) = potentialParent : insertSiblings (firstSibling : secondSibling : restOfPotentialParents)
       -- Parent found
-      | otherwise                                                                 = potentialParent { subForest = [firstSibling, secondSibling] } : restOfPotentialParents
+      | otherwise                                                                 = potentialParent { subForest = [secondSibling, firstSibling] } : restOfPotentialParents
     -- There are not 3 or more nodes
     insertSiblings nodes' = nodes'
 -- There are not 3 or more nodes
@@ -26,14 +33,26 @@ buildTree nodes = head nodes -- return the built tree from inside the list
 
 testSiblingPairs = [[2, 3], [-1, -1], [-1, -1]]
 testSiblingPairs2 = [[2, 3], [-1, 4], [-1, 5], [-1, -1], [-1, -1]]
-testReversedNodeList = (\a -> Node a []) <$> reverse (concat $ [1] : testSiblingPairs)
-testReversedNodeList2 = (\a -> Node a []) <$> reverse (concat $ [1] : testSiblingPairs2)
+testSiblingPairs3 = [[2, 3], [4, -1], [5, -1], [6, -1], [7, 8], [-1, 9], [-1, -1], [10, 11], [-1, -1], [-1, -1], [-1, -1]]
+testReversedNodeList = reversedNodeList testSiblingPairs
+testReversedNodeList2 = reversedNodeList testSiblingPairs2
+testReversedNodeList3 = reversedNodeList testSiblingPairs3
+
+leftToRightTraversal (Node id (firstChild : secondChild : _)) = leftToRightTraversal firstChild <> [id] <> leftToRightTraversal secondChild
+leftToRightTraversal (Node id (firstChild : _)) = leftToRightTraversal firstChild <> [id]
+leftToRightTraversal (Node id [])
+  | id == -1  = []
+  | otherwise = [id]
+
+-- leftToRightTraversal (Node id (firstChild : secondChild : _)) = leftToRightTraversal firstChild <> id <> leftToRightTraversal secondChild
+-- leftToRightTraversal (Node id (firstChild : _)) = leftToRightTraversal firstChild
+-- leftToRightTraversal (Node id []) = id
 
 -- -- A list representing the in order traversal of nodes of a given binary tree
 -- -- Example: (Node 1 [Node 2 [], Node 3 []]) = [2, 1, 3]
--- inOrderTraversal (Node id (firstChild : secondChild : _)) = inOrderTraversal firstChild <> show id <> inOrderTraversal secondChild
--- inOrderTraversal (Node id (firstChild : _)) = inOrderTraversal firstChild <> show id
--- inOrderTraversal (Node id []) = show id
+inOrderTraversal (Node id (firstChild : secondChild : _)) = [rootLabel firstChild] <> leftToRightTraversal firstChild <> [id] <> [rootLabel secondChild] <> leftToRightTraversal secondChild
+inOrderTraversal (Node id (firstChild : _)) = [rootLabel firstChild] <> leftToRightTraversal firstChild <> [id]
+inOrderTraversal (Node id []) = [id]
 
 -- -- Swap the nodes of a given tree at a given depth
 -- swapNodes depth tree = tree
