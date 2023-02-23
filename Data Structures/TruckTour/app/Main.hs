826a2feb8248c41707e0fad075778509e7fad87d
ChrisHobday <hobday.chris@protonmail.com>
2023-02-22 23:35:59 -0500

    Update TruckTour challenge, add skeleton code, add petrol pump datatype

--- Data Structures/TruckTour/app/Main.hs
+++ Data Structures/TruckTour/app/Main.hs
@@ -1,5 +1,22 @@
 module Main (main) where
 
+import Control.Monad ( replicateM )
+
+data PetrolPump =
+  PetrolPump
+    { amountOfPetrolToGive :: Int
+    , distanceToNextPump   :: Int
+    } deriving ( Show )
+
+-- The index of the first petrol pump from a given list of petrol pumps that can complete the tour (won't run out of petrol)
+firstPetrolPumpThatCanCompleteTour :: [PetrolPump] -> Int
+firstPetrolPumpThatCanCompleteTour = undefined
+
 main :: IO ()
 main = do
-  print "TruckTour"
+  numberOfPetrolPumps <- readLn :: IO Int -- Read and bind number of petrol pumps to be entered
+  petrolPumps <- replicateM numberOfPetrolPumps $ do -- For each petrol pump to be entered...
+    (amountOfPetrolToGive : distanceToNextPump : _) <- (read <$>) . words <$> getLine :: IO [Int] -- Read amount of petrol to give and distance to next pump
+    return $ PetrolPump amountOfPetrolToGive distanceToNextPump -- Return entered petrol pump
+
+  print petrolPumps
