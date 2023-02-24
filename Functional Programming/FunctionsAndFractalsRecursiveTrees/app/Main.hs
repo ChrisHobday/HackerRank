e2597491c69f727decb0d441b965497289e34d8b
ChrisHobday <hobday.chris@protonmail.com>
2023-02-24 01:27:34 -0500

    Update FunctionsAndFractalsRecursiveTrees challenge, add skeleton code

--- Functional Programming/FunctionsAndFractalsRecursiveTrees/app/Main.hs
+++ Functional Programming/FunctionsAndFractalsRecursiveTrees/app/Main.hs
@@ -1,5 +1,9 @@
 module Main (main) where
 
+recursiveTree numberOfIterations = ""
+
 main :: IO ()
 main = do
-  print "FunctionsAndFractalsRecursiveTrees"
+  numberOfIterations <- readLn :: IO Int -- Read and bind number of interations
+
+  print $ recursiveTree numberOfIterations -- Print the recursive tree with the entered number of interations
