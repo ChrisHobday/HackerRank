468865714a4d5069b7708822992897c11031dae4
ChrisHobday <hobday.chris@protonmail.com>
2023-02-24 19:48:35 -0500

    Update FunctionsAndFractalsRecursiveTrees challenge, add branch, branchLeft, branchRight functions

--- Functional Programming/FunctionsAndFractalsRecursiveTrees/app/Main.hs
+++ Functional Programming/FunctionsAndFractalsRecursiveTrees/app/Main.hs
@@ -1,6 +1,24 @@
 module Main (main) where
 
+branch (char : char' : char'' : chars)
+  | char' == '1' = '1' : '_' : '1' : branch chars
+  | otherwise    = char : branch (char' : char'' : chars)
+branch chars = chars
+
+branchLeft (char : char' : chars)
+  | char' == '1' = '1' : '_' : branchRight chars
+  | otherwise    = char : branchLeft (char' : chars)
+branchLeft chars = chars
+
+branchRight (char : char' : chars)
+  | char == '1' = '_' : '1' : branchLeft chars
+  | otherwise   = char : branchRight (char' : chars)
+branchRight chars = chars
+
 recursiveTree numberOfIterations = ""
+  where
+    numberOfRoots    = 2 ^ (numberOfIterations - 1)
+    numberOfBranches = 2 ^ numberOfIterations
 
 main :: IO ()
 main = do
