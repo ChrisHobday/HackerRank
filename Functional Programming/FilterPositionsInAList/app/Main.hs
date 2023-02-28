ae9475fda416da88578602d44262887264c9b50d
ChrisHobday <hobday.chris@protonmail.com>
2023-02-27 22:42:51 -0500

    Complete FilterPositionsInAList challenge

--- Functional Programming/FilterPositionsInAList/app/Main.hs
+++ Functional Programming/FilterPositionsInAList/app/Main.hs
@@ -1,7 +1,13 @@
+module Main (main) where
+
+-- -- A given list filtered to remove every other element starting with the first
 f :: [Int] -> [Int]
-f lst = undefined-- Fill up this Function
+f (oddElement : evenElement : otherElements) = evenElement : f otherElements
+f [oddElement]                               = []
+f []                                         = []
 
 -- This part deals with the Input and Output and can be used as it is. Do not modify it.
 main = do
-	inputdata <- getContents
-	mapM_ (putStrLn. show). f. map read. lines $ inputdata
+  inputdata <- getContents -- Read and bind list of numbers as string
+
+  mapM_ (putStrLn. show). f. map read. lines $ inputdata -- Convert entered string to list of numbers and print the result of filtering every other element
