277507002c025d554978ca07eba1fe6fd2d95bb9
ChrisHobday <hobday.chris@protonmail.com>
2023-02-28 16:14:15 -0500

    Complete ArrayOfNElements challenge, complete fn function

--- Functional Programming/ArrayOfNElements/app/Main.hs
+++ Functional Programming/ArrayOfNElements/app/Main.hs
@@ -1,7 +1,10 @@
 module Main (main) where
 
-fn n = undefined
+-- A list of numbers from 0 to 1 less than a given number
+fn :: (Num a, Enum a) => a -> [a]
+fn n = [0 .. n - 1]
 
+main :: IO ()
 main = do
   n <- readLn :: IO Int
   print (fn(n))
