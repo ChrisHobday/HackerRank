49fc3911e1b53e493d09788d6252d72c8ba046ea
ChrisHobday <hobday.chris@protonmail.com>
2023-03-01 16:56:00 -0500

    Complete UpdateList challenge, finish f function

--- Functional Programming/UpdateList/app/Main.hs
+++ Functional Programming/UpdateList/app/Main.hs
@@ -1,8 +1,9 @@
 module Main (main) where
 
--- Enter your code here. Read input from STDIN. Print output to STDOUT
-
-f arr = -- Complete this function here
+-- A list of absolute numbers from a given list of numbers
+f :: Num a => [a] -> [a]
+f (number : otherNumbers) = abs number : f otherNumbers -- Complete this function here
+f []                      = []
 
 -- This section handles the Input/Output and can be used as it is. Do not modify it.
 main = do
