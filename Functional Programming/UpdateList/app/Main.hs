78dbd5b730aee61dc3982d839a9140e992f58016
ChrisHobday <hobday.chris@protonmail.com>
2023-03-01 16:52:07 -0500

    Update UpdateList challenge, update package.yaml, remove lib.hs, add skeleton code, add problem statement and testcases

--- Functional Programming/UpdateList/app/Main.hs
+++ Functional Programming/UpdateList/app/Main.hs
@@ -1,6 +1,10 @@
 module Main (main) where
 
-import Lib
+-- Enter your code here. Read input from STDIN. Print output to STDOUT
 
-main :: IO ()
-main = someFunc
+f arr = -- Complete this function here
+
+-- This section handles the Input/Output and can be used as it is. Do not modify it.
+main = do
+	inputdata <- getContents
+	mapM_ putStrLn $ map show $ f $ map (read :: String -> Int) $ lines inputdata
