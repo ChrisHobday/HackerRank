93674d3fcfe6538719b4ebd69c4429214e89019a
ChrisHobday <hobday.chris@protonmail.com>
2023-03-01 17:01:55 -0500

    Update UpdateList challenge, clean up comments

--- Functional Programming/UpdateList/app/Main.hs
+++ Functional Programming/UpdateList/app/Main.hs
@@ -2,10 +2,10 @@ module Main (main) where
 
 -- A list of absolute numbers from a given list of numbers
 f :: Num a => [a] -> [a]
-f (number : otherNumbers) = abs number : f otherNumbers -- Complete this function here
+f (number : otherNumbers) = abs number : f otherNumbers
 f []                      = []
 
 -- This section handles the Input/Output and can be used as it is. Do not modify it.
 main = do
-	inputdata <- getContents
-	mapM_ putStrLn $ map show $ f $ map (read :: String -> Int) $ lines inputdata
+  inputdata <- getContents
+  mapM_ putStrLn $ map show $ f $ map (read :: String -> Int) $ lines inputdata
