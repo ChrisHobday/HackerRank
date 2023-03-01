2016e5c120493b903dc2c298e18f5f077ec0d719
ChrisHobday <hobday.chris@protonmail.com>
2023-03-01 16:47:41 -0500

    Complete ListLength challenge, finish len function

--- Functional Programming/ListLength/app/Main.hs
+++ Functional Programming/ListLength/app/Main.hs
@@ -1,4 +1,6 @@
 module Main (main) where
 
+-- The number of elements in a given list
 len :: [a] -> Int
-len lst = undefined
+len (element : otherElements) = 1 + len otherElements
+len []                        = 0
