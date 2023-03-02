7e8e52e0a21eeb0a9bc0586370a62b7f03b86659
ChrisHobday <hobday.chris@protonmail.com>
2023-03-02 16:42:52 -0500

    Update EvaluatingEToTheX challenge, add skeleton code

--- Functional Programming/EvaluatingEToTheX/app/Main.hs
+++ Functional Programming/EvaluatingEToTheX/app/Main.hs
@@ -1,5 +1,16 @@
 module Main (main) where
 
+import Control.Monad
+  ( replicateM 
+  , mapM_ )
+
+evaluate number = undefined
+
 main :: IO ()
 main = do
-  print "EvaluatingEToTheX"
+  numberOfNumberToBeEntered <- readLn :: IO Int -- Read and bind number of numbers to be entered
+
+  numbers <- replicateM numberOfNumberToBeEntered $ do -- For each number to be entered...
+    readLn :: IO Float -- Read number
+  
+  mapM_ evaluate numbers
