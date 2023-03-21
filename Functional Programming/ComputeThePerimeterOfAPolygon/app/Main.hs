eeaf5afa4ac923eb669a40f1163f12286bd240d7
ChrisHobday <hobday.chris@protonmail.com>
2023-03-20 22:53:25 -0400

    Update challenge ComputeThePerimeterOfAPolygon, add skeleton code

--- Functional Programming/ComputeThePerimeterOfAPolygon/app/Main.hs
+++ Functional Programming/ComputeThePerimeterOfAPolygon/app/Main.hs
@@ -1,4 +1,13 @@
 module Main (main) where
 
+import Control.Monad ( replicateM )
+
 main :: IO ()
-main = someFunc
+main = do
+  numberOfPointsToBeEntered <- readLn :: IO Int -- Read and bind number of points to be entered
+
+  points <- replicateM numberOfPointsToBeEntered $ do -- For each point to be entered...
+    (x : y : _) <- (read <$>) . words <$> getLine :: IO [Int] -- Read and bind the point to x and y
+    return (x, y) -- Return the point as (x, y)
+  
+  print points
