0dc602ea25b2654982d040b7e683db67144b4949
ChrisHobday <hobday.chris@protonmail.com>
2023-03-22 17:56:57 -0400

    Update ComputeTheAreaOfAPolygon challenge, add skeleton code

--- Functional Programming/ComputeTheAreaOfAPolygon/app/Main.hs
+++ Functional Programming/ComputeTheAreaOfAPolygon/app/Main.hs
@@ -1,5 +1,15 @@
 module Main (main) where
 
+import Control.Monad ( replicateM )
+
+area = undefined
+
 main :: IO ()
 main = do
-  print "ComputeTheAreaOfAPolygon"
+  numberOfPointsToBeEntered <- readLn :: IO Int -- Read and bind number of points to be entered
+
+  points <- replicateM numberOfPointsToBeEntered $ do -- For each point to be entered...
+    (x : y : _) <- (read <$>) . words <$> getLine :: IO [Float] -- Read and bind the point to x and y
+    return (x, y) -- Return the point as (x, y)
+  
+  print $ area points -- Print the area of entered polygon (as a list of points)
