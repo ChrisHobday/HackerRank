bebd10cee6234d80610aad28479ca97efa95ad7d
ChrisHobday <hobday.chris@protonmail.com>
2023-03-29 21:18:11 -0400

    Update FibonacciNumbers challenge, add skeleton code

--- Functional Programming/FibonacciNumbers/app/Main.hs
+++ Functional Programming/FibonacciNumbers/app/Main.hs
@@ -1,5 +1,9 @@
 module Main (main) where
 
+fib n = undefined
+
 main :: IO ()
 main = do
-  print ""
+  nFibonacciNumber <- readLn :: IO Int -- Read and bind the nth fibonacci number to calculate
+  
+  print $ fib nFibonacciNumber -- Print the nth fibonacci number to calculate
