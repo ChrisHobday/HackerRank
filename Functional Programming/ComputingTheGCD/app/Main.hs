858643d91c626dc8e05dc236a5628d34b640d930
ChrisHobday <hobday.chris@protonmail.com>
2023-04-04 23:31:00 -0400

    Complete ComputingTheGCD challenge, finish gcd' function, fix user input code, add comments

--- Functional Programming/ComputingTheGCD/app/Main.hs
+++ Functional Programming/ComputingTheGCD/app/Main.hs
@@ -1,8 +1,19 @@
 module Main (main) where
 
-gcd' x y = undefined
+-- The greatest common denominator of two gicen numbers
+-- Example: gcd' 45 10 = 5
+gcd' :: Integral t => t -> t -> t
+gcd' x y
+  -- If the remainder is 0
+  | remainder == 0 = smallerNumber
+  -- Otherwise the remainder is not 0
+  | otherwise      = gcd' smallerNumber remainder
+  where
+    smallerNumber    = min x y
+    biggerNumber     = max x y
+    (div, remainder) = divMod biggerNumber smallerNumber
 
 main :: IO ()
 main = do
-  (x : _ : y : _) <- (read <$>) . words <$> getLine :: IO [Int] -- Read two number to find GCD between and bind them to x and y
-  print $ gcd' x y
+  (x : y : _) <- (read <$>) . words <$> getLine :: IO [Int] -- Read two numbers to find GCD between and bind them to x and y
+  print $ gcd' x y -- Print the GCD of two entered numbers
