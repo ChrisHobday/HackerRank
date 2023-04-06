8e2d59ec1f3299475272207a7afc4358c7d5a7b8
ChrisHobday <hobday.chris@protonmail.com>
2023-04-06 16:36:51 -0400

    Update PascalsTriangle challenge, add skeleton code

--- Functional Programming/PascalsTriangle/app/Main.hs
+++ Functional Programming/PascalsTriangle/app/Main.hs
@@ -1,5 +1,9 @@
 module Main (main) where
 
+pascalTriangle numberOfRows = undefined
+
 main :: IO ()
 main = do
-  print ""
+  numberOfRows <- readLn :: IO Int -- Read and bind number of rows
+  
+  pascalTriangle numberOfRows -- Print the pascal triangle for the given number of rows
