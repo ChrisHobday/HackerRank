67a74d0584694ef0963bbac5c6d799ee549b2219
ChrisHobday <hobday.chris@protonmail.com>
2023-09-22 15:11:33 -0400

    Update PrefixCompression challenge, add numberOfSameCharacters to prefixCompression to improve efficiency

--- Functional Programming/PrefixCompression/app/Main.hs
+++ Functional Programming/PrefixCompression/app/Main.hs
@@ -2,20 +2,20 @@ module Main (main) where
 
 -- A prefix compression of 2 given strings
 prefixCompression :: [Char] -> [Char] -> [Char]
-prefixCompression string string2 = prefixCompression' "" string string2
+prefixCompression string string2 = prefixCompression' 0 "" string string2
   where
-    -- Subfunction to encapsulate sameCharacters
-    prefixCompression' sameCharacters string' string2'
+    -- Subfunction to encapsulate numberOfSameCharacters and sameCharacters
+    prefixCompression' numberOfSameCharacters sameCharacters string' string2'
       -- First or second string is empty
       | null string' || null string2' || firstCharacter /= firstCharacter2 =
         -- Check length of the same characters
-        case length sameCharacters of
+        case numberOfSameCharacters of
           -- There was not previously matching charactes
           0 -> show (length string') ++ " " ++ string' ++ "\n" ++ show (length string2') ++ " " ++ string2'
           -- There was previously matchiing characters
-          _ -> show (length sameCharacters) ++ " " ++ sameCharacters ++ "\n" ++ show (length string') ++ " " ++ string' ++ "\n" ++ show (length string2') ++ " " ++ string2'
+          _ -> show numberOfSameCharacters ++ " " ++ sameCharacters ++ "\n" ++ show (length string') ++ " " ++ string' ++ "\n" ++ show (length string2') ++ " " ++ string2'
       -- Otherwise (First character of both strings match)
-      | otherwise = prefixCompression' (sameCharacters ++ [firstCharacter]) restofString restofString2
+      | otherwise = prefixCompression' (numberOfSameCharacters + 1) (sameCharacters ++ [firstCharacter]) restofString restofString2
       where
         (firstCharacter : restofString)    = string'
         (firstCharacter2 : restofString2)  = string2'
