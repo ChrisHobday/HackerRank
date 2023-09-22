386a71f9ea95a0a0e7c25a93989a8ddb9a8bb9b7
ChrisHobday <hobday.chris@protonmail.com>
2023-09-22 14:54:22 -0400

    Update PrefixCompression challenge, finiish prefixCompression function

--- Functional Programming/PrefixCompression/app/Main.hs
+++ Functional Programming/PrefixCompression/app/Main.hs
@@ -1,4 +1,19 @@
 module Main (main) where
+  
+prefixCompression sameCharacters string string2
+  -- First or second string is empty
+  | null string || null string2 || firstCharacter /= firstCharacter2 =
+    -- Check length of the same characters
+    case length sameCharacters of
+      -- There was not previously matching charactes
+      0 -> show (length string) ++ " " ++ string ++ "\n" ++ show (length string2) ++ " " ++ string2
+      -- There was previously matchiing characters
+      _ -> show (length sameCharacters) ++ " " ++ sameCharacters ++ "\n" ++ show (length string) ++ " " ++ string ++ "\n" ++ show (length string2) ++ " " ++ string2
+  -- Otherwise (First character of both strings match)
+  | otherwise = prefixCompression (sameCharacters ++ [firstCharacter]) restofString restofString2
+  where
+    (firstCharacter : restofString)    = string
+    (firstCharacter2 : restofString2)  = string2
 
 main :: IO ()
 main = undefined
