7dcecded78c9ddfb03c8819c698e52ffe487e597
ChrisHobday <hobday.chris@protonmail.com>
2023-09-22 18:30:34 -0400

    Finish StringReductions challenge, add comments

--- Functional Programming/StringReductions/app/Main.hs
+++ Functional Programming/StringReductions/app/Main.hs
@@ -1,10 +1,16 @@
 module Main (main) where
 
+-- A given string with all duplicate characters removed
+stringReduction :: [Char] -> [Char]
 stringReduction string = stringReduction' "" string
   where
+    -- Subfunction to encapsulate usedCharacters
     stringReduction' usedCharacters string'
+      -- String is empty
       | null string'                    = reverse usedCharacters
+      -- Character is already in used characters
       | character `elem` usedCharacters = stringReduction' usedCharacters restOfCharacters
+      -- Otherwise (String is not empty and character iis not already used)
       | otherwise                       = stringReduction' (character : usedCharacters) restOfCharacters
       where
         (character : restOfCharacters) = string'
