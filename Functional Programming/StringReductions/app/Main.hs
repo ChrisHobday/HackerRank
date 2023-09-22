39d00dd7822250b3fd444e06f01921353e0b0728
ChrisHobday <hobday.chris@protonmail.com>
2023-09-22 18:27:16 -0400

    Update StringReductions challenge, add/finish stringReduction function, fiinish main input/output

--- Functional Programming/StringReductions/app/Main.hs
+++ Functional Programming/StringReductions/app/Main.hs
@@ -1,4 +1,15 @@
 module Main (main) where
 
+stringReduction string = stringReduction' "" string
+  where
+    stringReduction' usedCharacters string'
+      | null string'                    = reverse usedCharacters
+      | character `elem` usedCharacters = stringReduction' usedCharacters restOfCharacters
+      | otherwise                       = stringReduction' (character : usedCharacters) restOfCharacters
+      where
+        (character : restOfCharacters) = string'
+
 main :: IO ()
-main = undefined
+main = do
+  string <- getLine -- Read and bind string
+  putStrLn $ stringReduction string -- Print string reduction of entered string
