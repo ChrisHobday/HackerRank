81816e3f4d91cda624a63e7be2ea087ce1e89485
ChrisHobday <hobday.chris@protonmail.com>
2023-09-25 21:42:32 -0400

    Update StringOPermute challenge, add/finish swapEvenCharacters function, add/finish main input/output, add comments

--- Functional Programming/StringOPermute/app/Main.hs
+++ Functional Programming/StringOPermute/app/Main.hs
@@ -1,4 +1,23 @@
 module Main (main) where
 
+import Control.Monad ( replicateM )
+
+-- A given string with it's even characters swapped with the previous character
+-- Ex: swapEvenCharacters "abcde" = "badce"
+swapEvenCharacters :: [a] -> [a]
+swapEvenCharacters string
+  -- Length of given string is less than 2 (not enough characters to swap)
+  | length string < 2 = string
+  -- Otherwise (length of given string is not less than 2)
+  | otherwise         = char2 : char1 : swapEvenCharacters restOfChars
+  where
+    (char1 : char2 : restOfChars) = string
+
 main :: IO ()
-main = undefined
+main = do
+  numberOfTestCases <- readLn :: IO Int -- Read number of test cases to be entered and bind it
+  testCases <- replicateM numberOfTestCases $ do -- Replicate the following action for each test case
+    string <- getLine -- Read and bind the string to swap even characters on
+    return $ swapEvenCharacters string -- Return user entered string with characters swapped
+  mapM_ putStrLn testCases -- Print the list of given string with their even characters swapped (one per line)
+  
