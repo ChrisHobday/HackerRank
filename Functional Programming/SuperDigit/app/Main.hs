ab39c81ca2b48172e044dbde3be068c0c708144f
ChrisHobday <hobday.chris@protonmail.com>
2023-09-28 18:24:58 -0400

    Finish SuperDigit challenge, redo superDigit function to multiply and mod by 9 instead for much better efficiency, redo main i/o, redo comments

--- Functional Programming/SuperDigit/app/Main.hs
+++ Functional Programming/SuperDigit/app/Main.hs
@@ -1,21 +1,18 @@
 module Main (main) where
 
-import Data.List ( genericReplicate )
-
 -- A given number's digit summed together recursively until the number is only a single digit
 -- Ex: superDigit 1234 = 1
-superDigit :: (Ord t, Num t, Read t, Show t) => t -> t
-superDigit numberToSumDigitsOf
-  -- The summed digits is greater than 9 (more than 1 digit)
-  | summedDigits > 9 = superDigit summedDigits
-  -- Otherwise (The summed digits is 9 or less (only 1 digit))
-  | otherwise        = summedDigits
+superDigit :: Integral a => a -> a -> a
+superDigit number numberOfTimesToReplicate
+  -- Remainder is 0
+  | remainder == 0 = 9
+  -- Otherwise (Remainder is not 0)
+  | otherwise      = remainder
   where
-    -- The given number's digits summed together
-    summedDigits = sum (read <$> ((:"") <$> show numberToSumDigitsOf))
+    -- The remainder after modding the number multiplied by the number of times to replicate the number by 9
+    remainder = (number * numberOfTimesToReplicate) `mod` 9
 
 main :: IO ()
 main = do
   (number : numberOfTimesToReplicate : _) <- map read . words <$> getLine :: IO [Integer] -- Read and bind the number and number of times to replicate it
-  let numberToSumDigitsOf = read $ concat $ genericReplicate numberOfTimesToReplicate (show number) :: Integer -- Replicate the given number to given number of times and set it
-  print $ superDigit numberToSumDigitsOf -- Print the super digit of the given number, replicated the given number of times
+  print $ superDigit number numberOfTimesToReplicate -- Print the super digit of the given number with the number of times to replicate it
