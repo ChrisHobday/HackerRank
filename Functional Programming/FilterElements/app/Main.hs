de23af30305b872fa92fd5a3805be16dd372340e
ChrisHobday <hobday.chris@protonmail.com>
2023-10-03 15:58:08 -0400

    Update FilterElements challenge, update program to use IntMaps instead of Maps for better efficiency (Note: still does not give correcting ordering on input)

--- Functional Programming/FilterElements/app/Main.hs
+++ Functional Programming/FilterElements/app/Main.hs
@@ -1,17 +1,18 @@
 module Main (main) where
 
-import qualified Data.Map as Map
+import qualified Data.IntMap as Map
 import Control.Monad ( replicateM )
 
 -- The number of times each integer occurs in a given list (as a key-value map)
 -- Ex: integerOccurencesMap [1,2,3,1] Map.empty = fromList [(1,2),(2,1),(3,1)]
--- integerOccurencesMap :: (Foldable t, Ord k, Num a) => t k -> Map.HashMap k a -> Map.HashMap k a
-integerOccurencesMap :: (Foldable t, Ord k, Num a) => t k -> Map.Map k a -> Map.Map k a
-integerOccurencesMap integers map = foldl (\map' integer' -> Map.insertWith (+) integer' 1 map') map integers
+-- integerOccurencesMap :: (Foldable t, Ord k, Num a) => t k -> Map.Map k a -> Map.Map k a
+integerOccurencesMap :: (Foldable t, Num a) => t Map.Key -> Map.IntMap a -> Map.IntMap a
+integerOccurencesMap integers integerMap = foldl (\map' integer' -> Map.insertWith (+) integer' 1 map') integerMap integers
 
 -- A list of integers the occur the given amount of times
 -- Ex: filterElements 2 [1,1,2,3,2] = [1,2]
-filterElements :: (Foldable t, Ord a, Ord k, Num a) => a -> t k -> [k]
+-- filterElements :: (Foldable t, Ord a, Ord k, Num a) => a -> t k -> [k]
+filterElements :: (Ord a, Foldable t, Num a) => a -> t Map.Key -> [Map.Key]
 filterElements repetitionCount integers = Map.keys $ Map.filter (>= repetitionCount) $ integerOccurencesMap integers Map.empty
 
 main :: IO ()
