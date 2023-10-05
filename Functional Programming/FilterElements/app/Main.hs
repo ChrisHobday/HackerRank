cc0f60e6be6199abd6e99bf78693b9c25abd4be6
ChrisHobday <hobday.chris@protonmail.com>
2023-10-04 23:19:18 -0400

    Update FilterElements challenge, add/finish integersThatOccurEnough function which looksup a given list of integer keys to see if there values occur at least the given amount of times (this is needed because maps automatically sort and produce the wrong ordered output when used alone), update filterElements function to use new integersThatOccurEnough function, add comments

--- Functional Programming/FilterElements/app/Main.hs
+++ Functional Programming/FilterElements/app/Main.hs
@@ -1,36 +1,39 @@
 module Main (main) where
 
+import qualified Data.Map as Map
+import Data.Maybe ( isJust, fromJust )
+import Data.List ( nub )
 import Control.Monad ( replicateM )
 
--- Add a given integer to a given list of occurences or update the occurence counter for that given integer
--- Ex: addToIntegerList 5 [(1,1),(5,3)] = [(1,1),(5,4)]
-addToIntegerList :: (Eq t, Num b) => t -> [(t, b)] -> [(t, b)]
-addToIntegerList integer ((integer', numberOfOccurences) : integers)
-  -- The given integer has been found in the given list of integers
-  | integer == integer' = (integer', numberOfOccurences + 1) : integers
-  -- Otherwise, the given integer has not found in the given list of integers (keep looking)
-  | otherwise           = (integer', numberOfOccurences) : addToIntegerList integer integers
--- The given integer is not in the given list of integers
-addToIntegerList integer [] = [(integer, 1)]
+-- Note: This solution uses key-value maps for tallying up the number of occurences for each integer, but also has to compare the map with a nubbed list of the integers because of a quirk with maps
+-- where they automatically sort the keys and as a result produce the wrong order when trying to output them.
 
--- A list of the number of times a given list of integers each occurs
--- integerOccurencesList [1,1,2,5] [] = [(1,2),(2,1),(5,1)]
-integerOccurencesList :: (Eq t, Num b) => [t] -> [(t, b)] -> [(t, b)]
-integerOccurencesList (integer : restOfIntegers) integerList = integerOccurencesList restOfIntegers $ addToIntegerList integer integerList
-integerOccurencesList [] integerList                         = integerList
+-- The number of times each integer occurs in a given list (as a key-value map)
+-- Ex: integerOccurencesMap [1,2,3,1] Map.empty = fromList [(1,2),(2,1),(3,1)]
+integerOccurencesMap :: (Foldable t, Ord k, Num a) => t k -> Map.Map k a -> Map.Map k a
+integerOccurencesMap integers integerMap = foldl (\integerMap' integer' -> Map.insertWith (+) integer' 1 integerMap') integerMap integers
 
--- A list of integers who have the required number of occurences from a given list of integers and their number of occurences
--- Ex: removeIntegers 2 [(1,3),(2,1)] = [1]
-removeIntegers :: Ord t => t -> [(a, t)] -> [a]
-removeIntegers repetitionCount ((integer, numberOfOccurences) : resotOfIntegerOccurences)
-  | numberOfOccurences >= repetitionCount = integer : removeIntegers repetitionCount resotOfIntegerOccurences
-  | otherwise                             = removeIntegers repetitionCount resotOfIntegerOccurences
-removeIntegers _ [] = []
+-- A list of integers from a given list that occur at least the given amount of times lookuped up from a given map
+-- Ex: integersThatOccurEnough 2 [1,2,3] (Map.fromList[(1,2),(2,0),(3,5)]) = [1,3]
+integersThatOccurEnough :: (Ord t, Ord a) => t -> [a] -> Map.Map a t -> [a]
+-- There is at least 1 integer in the given list
+integersThatOccurEnough repetitionCount (integer : restOfIntegers) integerMap
+  -- The value of the lookedup integer exists and is greater than or equal to the given repetition count
+  | isJust maybeIntegerNumberOfOccurences && integerNumberOfOccurences >= repetitionCount = integer : integersThatOccurEnough repetitionCount restOfIntegers integerMap
+  -- Otherwise, the value of the lookedup integer does not exists or is not greater than or equal to the given repetition count
+  | otherwise                                                                             = integersThatOccurEnough repetitionCount restOfIntegers integerMap
+  where
+    -- Maybe the value of the lookedup integer key
+    maybeIntegerNumberOfOccurences = Map.lookup integer integerMap
+    -- The value of the lookedup integer key
+    integerNumberOfOccurences      = fromJust maybeIntegerNumberOfOccurences
+-- There are no integers to check the occurences of
+integersThatOccurEnough _ [] _ = []
 
--- A list of integers from a given list of integers that occur at least the given amount of times
--- filterElements 2 [1,1,2,4,5,4,4] = [1,4]
-filterElements :: (Ord a, Num a, Eq b) => a -> [b] -> [b]
-filterElements repetitionCount integers = removeIntegers repetitionCount (integerOccurencesList integers [])
+-- A list of integers the occur the given amount of times
+-- Ex: filterElements 2 [1,1,2,3,2] = [1,2]
+filterElements :: (Ord t, Ord a, Num t) => t -> [a] -> [a]
+filterElements repetitionCount integers = integersThatOccurEnough repetitionCount (nub integers) (integerOccurencesMap integers Map.empty)
 
 main :: IO ()
 main = do
