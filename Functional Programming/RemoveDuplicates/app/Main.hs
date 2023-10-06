99b6c7ba6d22f6df412e51d480064d54f102e9d4
ChrisHobday <hobday.chris@protonmail.com>
2023-10-06 17:41:36 -0400

    Update/Finish RemoveDuplicates challenge, update/finish main i/o, add comments

--- Functional Programming/RemoveDuplicates/app/Main.hs
+++ Functional Programming/RemoveDuplicates/app/Main.hs
@@ -1,4 +1,8 @@
 module Main (main) where
 
+import Data.List ( nub )
+
 main :: IO ()
-main = undefined
+main = do
+  string <- getLine -- Read and bind string of characters to remove duplicates from
+  putStrLn $ nub string -- Print given string of characters with duplicates removed
