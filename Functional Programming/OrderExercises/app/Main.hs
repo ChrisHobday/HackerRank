c7ad15e7c3c1024c309bc8fd71d1cb8c2399ec15
ChrisHobday <hobday.chris@protonmail.com>
2023-10-20 16:57:28 -0400

    Update OrderExercises challenge, update firstIntegerSequenceSumsAndLeftovers and allSequenceSumsAndLeftovers to include unused integers in leftover list, finish sequenceSums function, add comments (Note: While this solution works it is too slow to pass all tests)

--- Functional Programming/OrderExercises/app/Main.hs
+++ Functional Programming/OrderExercises/app/Main.hs
@@ -1,28 +1,40 @@
 module Main (main) where
 
-import Control.Monad
-  ( mapM_ )
+import Data.List
+  ( intercalate )
 
--- The sequence sums and leftover lists for the first integer of a given list onward
--- Ex: firstIntegerSequenceSumsAndLeftovers 0 [1,2,3] = [(1,[2,3]),(3,[3]),(6,[])]
-firstIntegerSequenceSumsAndLeftovers :: Num t => t -> [t] -> [(t, [t])]
-firstIntegerSequenceSumsAndLeftovers _ [] = []
-firstIntegerSequenceSumsAndLeftovers previousSum (integer : integers) = (currentSum, integers) : firstIntegerSequenceSumsAndLeftovers currentSum integers
+-- The sequence sums and leftover lists for the first integer of a given list onward (given unusedIntegers are prepended to the list of leftovers)
+-- Ex: firstIntegerSequenceSumsAndLeftovers [] 0 [1,2,3] = [(1,[2,3]),(3,[3]),(6,[])]
+firstIntegerSequenceSumsAndLeftovers :: Num t => [t] -> t -> [t] -> [(t, [t])]
+-- There are no integers in given list
+firstIntegerSequenceSumsAndLeftovers _ _ []                                          = []
+-- There is at least 1 integer in given list
+firstIntegerSequenceSumsAndLeftovers unusedIntegers previousSum (integer : integers) = (currentSum, unusedIntegers <> integers) : firstIntegerSequenceSumsAndLeftovers unusedIntegers currentSum integers
   where
+    -- The current sequence sum
     currentSum = previousSum + integer
 
--- TODO find a way to keep previous integers not used in calculating the sum prepended to the front of leftovers list
--- The sequence sums and leftover lists for all integers of a given list onward
-allSequenceSumsAndLeftovers :: Num t => [t] -> [(t, [t])]
-allSequenceSumsAndLeftovers [] = []
-allSequenceSumsAndLeftovers allIntegers@(_ : restOfIntegers) = firstIntegerSequenceSumsAndLeftovers 0 allIntegers <> allSequenceSumsAndLeftovers restOfIntegers
+-- The sequence sums and leftover lists for all integers of a given list onward (given unusedIntegers are passed along and eventually prepended to the list of leftovers created in firstIntegerSequenceSumsAndLeftovers)
+-- Ex: allSequenceSumsAndLeftovers [] [1,2,3] = [(1,[2,3]),(3,[3]),(6,[]),(2,[1,3]),(5,[1]),(3,[1,2])]
+allSequenceSumsAndLeftovers :: Num t => [t] -> [t] -> [(t, [t])]
+-- There are no integers in given list
+allSequenceSumsAndLeftovers _ []                                                        = []
+-- There is at least 1 integer in given list
+allSequenceSumsAndLeftovers unusedIntegers allIntegers@(unusedInteger : restOfIntegers) = firstIntegerSequenceSumsAndLeftovers unusedIntegers 0 allIntegers <> allSequenceSumsAndLeftovers (unusedIntegers <> [unusedInteger]) restOfIntegers
 
-sequenceSums integers = undefined
+-- A list of maximum sum sequences of a given list of integers (up to the size of a given maximumNumberOfSums)
+-- Ex: sequenceSums [1,2,3] 2 = [6]
+sequenceSums :: (Ord a, Num t, Num a, Eq t) => [a] -> t -> [a]
+sequenceSums integers maximumNumberOfSums
+  -- The maximumNumber of sums has been reached or there are no integers in given list or the maximum sum sequence is negative
+  | maximumNumberOfSums == 0 || null integers || signum maximumSumSequence == (-1) = []
+  | otherwise                                                                      = maximumSumSequence : sequenceSums leftovers (maximumNumberOfSums - 1)
+  where
+    -- The maximum sum sequence along with the leftover integer list
+    (maximumSumSequence, leftovers) = maximum $ allSequenceSumsAndLeftovers [] integers
 
 main :: IO ()
 main = do
-  (_ : _ : maximumNumberOfSums : _) <- (read <$>) . words <$> getLine :: IO [Int] -- Read and bind the maximum number of sums to output (ignore the number of integers to be entered as we do not need it)
+  (_ : maximumNumberOfSums : _) <- (read <$>) . words <$> getLine :: IO [Int] -- Read and bind the maximum number of sums to output (ignore the number of integers to be entered as we do not need it)
   integers <- (read <$>) . words <$> getLine :: IO [Int] -- Read and bind the list of integers to calculate the list of sequence of sums of
-  print integers
-  -- print $ sequenceSums integers
-  -- mapM_ putStrLn sequenceSums
+  putStrLn $ intercalate "\n" $ show <$> sequenceSums integers maximumNumberOfSums -- Print the sequence of sums of a given list of integers (up to the size of a given maximumNumberOfSums)
