de4b991134cd79fc59e625cae435f3af055cfcb3
ChrisHobday <hobday.chris@protonmail.com>
2023-11-13 22:28:22 -0500

    Update OrderExercises challenge,add collapse and lock functions to help in getting all sums in one pass

--- Functional Programming/OrderExercises/app/Main.hs
+++ Functional Programming/OrderExercises/app/Main.hs
@@ -2,33 +2,112 @@ module Main (main) where
 
 import Data.List
   ( sortBy )
+import Data.Maybe
+  ( listToMaybe )
 
-positiveSequences currentSum currentSums bestSum bestSums (number : numbers)
-  -- Current number is negative and there is a best sum
-  | number < 0 && bestSum > 0 = positiveSequences 0 (currentSum : currentSums) 0 (bestSum : bestSums) numbers
+-- collapse (currentSum : currentSums) (bestSum : bestSums)
+--   -- This current best sum should be collapsed
+--   | currentSum - bestSum > 0 = [currentSum]
+--   -- | currentSum - bestSum > 0 = ([], currentSum)
+--   -- This current best sum should not be collapsed
+--   | otherwise                = bestSum : collapse currentSums bestSums
+-- collapse [] _ = []
+-- collapse _ [] = []
 
-positiveSequences :: (Num a, Ord a) => a -> a -> [a] -> [a]
-positiveSequences currentSum bestSum (number : numbers)
-  -- | newCurrentSum > currentSum && bestSum > 0 = newBestSum : positiveSequences 0 0 numbers
-  | newCurrentSum <= 0 && bestSum > 0         = newBestSum : positiveSequences 0 0 numbers
-  | otherwise                                 = positiveSequences newCurrentSum newBestSum numbers
+collapse (currentSum : currentSums) (bestSum : bestSums)
+  -- This current best sum should be collapsed
+  | currentSum - bestSum > 0 = ([], [currentSum])
+  -- | currentSum - bestSum > 0 = ([], currentSum)
+  -- This current best sum should not be collapsed
+  | otherwise                = (\(a, b) (as, bs) -> (a : as, b : bs)) (currentSum, bestSum) (collapse currentSums bestSums)
+collapse [] _ = ([], [])
+collapse (currentSum: currentSums) [] = ([currentSum], [])
+
+-- lock (currentSum : currentSums) (potentialBestSum : potentialBestSums) bestSums
+--   | currentSum < 0 = lock currentSums
+
+positiveSequences previousNumberNegative currentSums bestSums (number : numbers) = positiveSequences numberNegative newCurrentSums newBestSums numbers                       
   where
-    newCurrentSum =
-      if number > (currentSum + number)
-      then
-        number
+    numberNegative = number < 0
+    (newCurrentSums, newBestSums) =
+      if numberNegative then
+        if previousNumberNegative then
+        -- Negative negative
+          ((+ number) <$> currentSums, bestSums)
+        else
+        -- Positive negative
+          ((+ number) <$> currentSums, bestSums <> [last currentSums])
       else
-        currentSum + number
-    newBestSum = max newCurrentSum bestSum
-positiveSequences _ bestSum []
-  | bestSum > 0 = [bestSum]
-  | otherwise   = []
+        if previousNumberNegative then
+        -- Negative positive (or first time being executed)
+        collapse (((+ number) <$> currentSums) <> [number]) bestSums
+        else
+        -- Positive positive
+        collapse ((+ number) <$> currentSums) bestSums
 
-sequenceSums :: (Num a, Ord a) => [a] -> Int -> [a]
-sequenceSums numbers maximumNumberOfSums = take maximumNumberOfSums (sortBy (flip compare) $ positiveSequences 0 0 numbers)
+    -- newCurrentSums =
+    --   -- Sequence has gone from negative to positive or is the first time being executed
+    --   if not numberNegative && previousNumberNegative
+    --   then
+    --     number : ((+ number) <$> currentSums)
+    --   else
+    --     -- Check current sums to potentially lock some
+    --     (+ number) <$> currentSums
+    -- newBestSums =
+    --   if numberNegative
+    --   then
+    --     if not previousNumberNegative
+    --     then
+    --       head currentSums : bestSums
+    --     else
+    --       bestSums
+    --   else
+    --     -- Check best sums to potentially collapse some
+    --     collapse (tail newCurrentSums) bestSums
 
-main :: IO ()
-main = do
-  (_ : maximumNumberOfSums : _) <- (read <$>) . words <$> getLine :: IO [Int] -- Read and bind the maximum number of sums to output (ignore the number of numbers to be entered as we do not need it)
-  numbers <- (read <$>) . words <$> getLine :: IO [Int] -- Read and bind the list of numbers to calculate the list of sequence of sums of
-  mapM_ putStrLn (show <$> sequenceSums numbers maximumNumberOfSums) -- Print the sequence sums of the given list of numbers and maximum number of sums
+      -- -- Sequence has gone from positive to negative
+      -- if number < 0 && not previousNumberNegative
+      -- then
+      --   head currentSums : bestSums
+      -- else
+      --   -- Check best sums to potentially collapse some
+      --   bestSums
+
+positiveSequences previousNumberNegative currentSums bestSums [] = show previousNumberNegative <> show currentSums <> show bestSums <> show newBestSums
+  where
+    newBestSums =
+      -- There is a best sum in current sums to add to the list
+      if not previousNumberNegative && not (null currentSums) then
+          last currentSums : bestSums
+      else
+        bestSums
+
+main = return ()
+
+
+
+-- positiveSequences :: (Num a, Ord a) => a -> a -> [a] -> [a]
+-- positiveSequences currentSum bestSum (number : numbers)
+--   -- | newCurrentSum > currentSum && bestSum > 0 = newBestSum : positiveSequences 0 0 numbers
+--   | newCurrentSum <= 0 && bestSum > 0         = newBestSum : positiveSequences 0 0 numbers
+--   | otherwise                                 = positiveSequences newCurrentSum newBestSum numbers
+--   where
+--     newCurrentSum =
+--       if number > (currentSum + number)
+--       then
+--         number
+--       else
+--         currentSum + number
+--     newBestSum = max newCurrentSum bestSum
+-- positiveSequences _ bestSum []
+--   | bestSum > 0 = [bestSum]
+--   | otherwise   = []
+
+-- sequenceSums :: (Num a, Ord a) => [a] -> Int -> [a]
+-- sequenceSums numbers maximumNumberOfSums = take maximumNumberOfSums (sortBy (flip compare) $ positiveSequences 0 0 numbers)
+
+-- main :: IO ()
+-- main = do
+--   (_ : maximumNumberOfSums : _) <- (read <$>) . words <$> getLine :: IO [Int] -- Read and bind the maximum number of sums to output (ignore the number of numbers to be entered as we do not need it)
+--   numbers <- (read <$>) . words <$> getLine :: IO [Int] -- Read and bind the list of numbers to calculate the list of sequence of sums of
+--   mapM_ putStrLn (show <$> sequenceSums numbers maximumNumberOfSums) -- Print the sequence sums of the given list of numbers and maximum number of sums
