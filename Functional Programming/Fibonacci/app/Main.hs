0c0b4b8e3ce618ec5bcef8e7174af5feeef0266d
ChrisHobday <hobday.chris@protonmail.com>
2023-11-27 21:35:35 -0500

    Update Fibonacci challenge, add fib function to calculate fibonacci numbers, add modification to be applied to fib n, and add main to get number of testcases, fib numbers and print the output

--- Functional Programming/Fibonacci/app/Main.hs
+++ Functional Programming/Fibonacci/app/Main.hs
@@ -1,4 +1,18 @@
 module Main (main) where
 
+import Control.Monad ( replicateM )
+
+modification = 100000007
+
+fib 1 = 0
+fib 2 = 1
+fib n = fib (n - 1) + fib (n - 2)
+
 main :: IO ()
-main = undefined
+main = do
+  numberOfTestCases <- readLn :: IO Int -- Read and bind the number of test cases about to be entered
+
+  fibonacciNumbers <- replicateM numberOfTestCases $ do -- For each test case to be entered...
+    readLn :: IO Int -- Read and bind the fibonacci number to find
+  
+  mapM_ (print . (`mod` modification) .fib) fibonacciNumbers
