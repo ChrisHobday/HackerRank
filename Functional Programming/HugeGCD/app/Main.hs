61b39727c27b672bb2d803eeec803f12a569169c
ChrisHobday <hobday.chris@protonmail.com>
2023-11-29 01:15:41 -0500

    Finish/Update HugeGCD challenge, update main to read user input and print the GCD

--- Functional Programming/HugeGCD/app/Main.hs
+++ Functional Programming/HugeGCD/app/Main.hs
@@ -1,4 +1,12 @@
 module Main (main) where
 
 main :: IO ()
-main = undefined
+main = do
+  _ <- readLn :: IO Int -- Read integer and ignore it
+  ns <- (read <$>) . words <$> getLine :: IO [Integer] -- Read list of integers that will be multiplied together to form n
+  _ <- readLn :: IO Int -- Read integer and ignore it
+  ms <- (read <$>) . words <$> getLine :: IO [Integer] -- Read list of integers that will be multiplied together to form m
+  let n = product ns -- The product of the given ns
+      m = product ms -- The product of the given ns
+
+  print $ gcd n m `mod` 1000000007 -- Print the greatest common denominator of n and m modulo 1000000007
