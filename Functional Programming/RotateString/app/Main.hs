58b8399887d654d8f97bafdb39372982c29937f2
ChrisHobday <hobday.chris@protonmail.com>
2023-11-29 15:53:31 -0500

    Update RotateString challenge, add rotate function which moves the first character of a given string to the back, add rotateN function which rotates a given string it's length times and returns a list of strings for each rotation,update main function to read user input and print the correct rotations

--- Functional Programming/RotateString/app/Main.hs
+++ Functional Programming/RotateString/app/Main.hs
@@ -1,4 +1,19 @@
 module Main (main) where
 
+import Control.Monad ( replicateM
+                     , mapM_ )
+
+rotate (char : chars) = chars <> [char]
+
+rotateN chars = take n $ drop 1 $ iterate rotate chars
+  where n = length chars
+
 main :: IO ()
-main = undefined
+main = do
+  nStrings <- readLn :: IO Int
+
+  stringRotations <- replicateM nStrings $ do
+    string <- getLine
+    return $ rotateN string
+  
+  mapM_ (putStrLn . unwords) stringRotations
