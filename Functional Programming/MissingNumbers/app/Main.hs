708e0833b5f45f1bda0fd4bebbc1cc0e2a89cf77
ChrisHobday <hobday.chris@protonmail.com>
2023-12-04 18:16:27 -0500

    Update MissingNumbers challenge, add comments

--- Functional Programming/MissingNumbers/app/Main.hs
+++ Functional Programming/MissingNumbers/app/Main.hs
@@ -7,9 +7,9 @@ import Data.List
 
 main :: IO ()
 main = do
-  _ <- readLn :: IO Int
-  ns <- (read <$>) . words <$> getLine :: IO [Int]
-  _ <- readLn :: IO Int
-  ms <- (read <$>) . words <$> getLine :: IO [Int]
+  _ <- readLn :: IO Int -- Ignore user entered integer
+  ns <- (read <$>) . words <$> getLine :: IO [Int] -- Read and bind the first list of user entered integers
+  _ <- readLn :: IO Int -- Ignore user entered integer
+  ms <- (read <$>) . words <$> getLine :: IO [Int] -- Read and bind the second list of user entered integers
 
-  putStrLn $ unwords $ show <$> sort (nub $ ms \\ ns)
+  putStrLn $ unwords $ show <$> sort (nub $ ms \\ ns) -- Print each unqiue integer in the second list but not the first, sorted
