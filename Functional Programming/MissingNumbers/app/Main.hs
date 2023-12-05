2340963bf28e427727a7b5776c96d735624cf281
ChrisHobday <hobday.chris@protonmail.com>
2023-12-04 20:47:17 -0500

    Update MissingNumbers challenge, add comments

--- Functional Programming/MissingNumbers/app/Main.hs
+++ Functional Programming/MissingNumbers/app/Main.hs
@@ -2,12 +2,20 @@ module Main (main) where
 
 import qualified Data.Map as M
 
+-- A given dictionary/map with either 1 added to the given key or if the given key does not exist, create it and set it's value to 1
+-- Example: positiveDictionary M.empty 3 = fromList [(3, 1)]
+-- Example: positiveDictionary (M.fromList [(3, 50)]) 3 = fromList [(3,51)]
 positiveDictionary :: (Ord k, Num a, Eq a) => M.Map k a -> k -> M.Map k a
 positiveDictionary dictionary m = M.alter (\x -> if x == Nothing then Just 1 else (+ 1) <$> x) m dictionary
 
+-- A given dictionary/map with either 1 subtracted to the given key or if the given key does not exist, create it and set it's value to -1
+-- Example: negativeDictionary M.empty 3 = fromList [(3,-1)]
+-- Example: negativeDictionary (M.fromList [(3, 50)]) 3 = fromList [(3,49)]
 negativeDictionary :: (Ord k, Num a, Eq a) => M.Map k a -> k -> M.Map k a
 negativeDictionary dictionary n = M.alter (\x -> if x == Nothing then Just (-1) else (+ (-1)) <$> x) n dictionary
 
+-- A dictionary with a running tally of the occurences of a given list of numbers minus another given list of numbers
+-- Example: missingNumbers M.empty [203,204,205] [203,204,205,203] = fromList [(203,1),(204,0),(205,0)]
 missingNumbers :: (Ord a1, Num a2, Eq a2) => M.Map a1 a2 -> [a1] -> [a1] -> M.Map a1 a2
 missingNumbers dictionary (n : ns) (m : ms) = missingNumbers newDictionary ns ms
   where
