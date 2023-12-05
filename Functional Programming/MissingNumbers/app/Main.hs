8f6daf968664d27cc1f84d93ba23e907b9ea49f2
ChrisHobday <hobday.chris@protonmail.com>
2023-12-04 20:37:03 -0500

    Update MissingNumbers challenge, switch to using maps/dictionaries to solve more efficiently

--- Functional Programming/MissingNumbers/app/Main.hs
+++ Functional Programming/MissingNumbers/app/Main.hs
@@ -1,9 +1,20 @@
 module Main (main) where
 
-import Data.List
-  ( sort
-  , nub
-  , (\\) )
+import qualified Data.Map as M
+
+positiveDictionary :: (Ord k, Num a, Eq a) => M.Map k a -> k -> M.Map k a
+positiveDictionary dictionary m = M.alter (\x -> if x == Nothing then Just 1 else (+ 1) <$> x) m dictionary
+
+negativeDictionary :: (Ord k, Num a, Eq a) => M.Map k a -> k -> M.Map k a
+negativeDictionary dictionary n = M.alter (\x -> if x == Nothing then Just (-1) else (+ (-1)) <$> x) n dictionary
+
+missingNumbers :: (Ord a1, Num a2, Eq a2) => M.Map a1 a2 -> [a1] -> [a1] -> M.Map a1 a2
+missingNumbers dictionary (n : ns) (m : ms) = missingNumbers newDictionary ns ms
+  where
+    newDictionary = positiveDictionary (negativeDictionary dictionary n) m
+missingNumbers dictionary [] (m : ms) = missingNumbers (positiveDictionary dictionary m) [] ms
+missingNumbers dictionary (n : ns) [] = missingNumbers (negativeDictionary dictionary n) ns []
+missingNumbers dictionary [] []       = dictionary
 
 main :: IO ()
 main = do
@@ -12,4 +23,4 @@ main = do
   _ <- readLn :: IO Int -- Ignore user entered integer
   ms <- (read <$>) . words <$> getLine :: IO [Int] -- Read and bind the second list of user entered integers
 
-  putStrLn $ unwords $ show <$> sort (nub $ ms \\ ns) -- Print each unqiue integer in the second list but not the first, sorted
+  putStrLn $ unwords $ show <$> M.keys (M.filter (> 0) $ missingNumbers M.empty ns ms) -- Print each unqiue integer in the second list but not the first, sorted
