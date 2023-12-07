462cef75aacacc549e210db908e5bb11a6138f98
ChrisHobday <hobday.chris@protonmail.com>
2023-12-07 18:01:20 -0500

    Update DifferentWays challenge, add count function to get the number of ways to choose k from n, update main to read the number of testcases and each test case and print the counts of each

--- Functional Programming/DifferentWays/app/Main.hs
+++ Functional Programming/DifferentWays/app/Main.hs
@@ -1,4 +1,21 @@
 module Main (main) where
 
+import Control.Monad
+  ( replicateM
+  , mapM_ )
+
+count _ 0 = 1
+count n k
+  | n == k    = 1
+  | n < k     = 1
+  | otherwise = count (n - 1) (k - 1) + count (n - 1) k
+
 main :: IO ()
-main = undefined
+main = do
+  testCases <- readLn :: IO Int
+
+  counts <- replicateM testCases $ do
+    (n : k : _) <- (read <$>) . words <$> getLine :: IO [Int]
+    return (count n k)
+  
+  mapM_ print counts
