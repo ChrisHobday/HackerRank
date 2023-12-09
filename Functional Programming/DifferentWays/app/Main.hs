2a36367e88bd70b532f00f6d058c1104f87fa6fc
ChrisHobday <hobday.chris@protonmail.com>
2023-12-08 23:07:55 -0500

    Finish DifferentWays challenge, add factorial function, add binomialCoefficient function to fast choose k from n, update count function to implement the binomialCoefficient function and apply the mod, update main to use Integers for bigger numbers, add comments

--- Functional Programming/DifferentWays/app/Main.hs
+++ Functional Programming/DifferentWays/app/Main.hs
@@ -4,18 +4,28 @@ import Control.Monad
   ( replicateM
   , mapM_ )
 
-count _ 0 = 1
-count n k
-  | n == k    = 1
-  | n < k     = 1
-  | otherwise = count (n - 1) (k - 1) + count (n - 1) k
+-- The factorial of a given number
+-- Ex: factorial 3 = 3 * 2 * 1 = 6
+factorial :: (Eq t, Num t) => t -> t
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+-- The binomial coefficient of two given numbers (choose k from n)
+-- Ex: binomialCoefficient 8 3 = 56
+binomialCoefficient :: Integral a => a -> a -> a
+binomialCoefficient n k = factorial n `div` (factorial k * factorial (n - k))
+
+-- The binomial coefficient of two given numbers (choose k from n) mod 100000007
+-- Ex: count 1000 5 = 90672686
+count :: Integral a => a -> a -> a
+count n k = binomialCoefficient n k `mod` 100000007
 
 main :: IO ()
 main = do
-  testCases <- readLn :: IO Int
+  testCases <- readLn :: IO Int -- Read and bind the number of test cases to be entered
 
-  counts <- replicateM testCases $ do
-    (n : k : _) <- (read <$>) . words <$> getLine :: IO [Int]
-    return (count n k)
+  counts <- replicateM testCases $ do -- For each test case do the following and bind the results (in the form of a list)
+    (n : k : _) <- (read <$>) . words <$> getLine :: IO [Integer] -- Read and bind n and k (choose k from n)
+    return (count n k) -- Return the count of n and k
   
-  mapM_ print counts
+  mapM_ print counts -- Print each count from the entered n and k's in each test case (on it's own line)
