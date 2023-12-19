606033b5a86d26709118b74906824b4c87659dfd
ChrisHobday <hobday.chris@protonmail.com>
2023-12-18 23:20:55 -0500

    Finish SubsetSum challenge, implement faster Data.Set solution

--- Functional Programming/SubsetSum/app/Main.hs
+++ Functional Programming/SubsetSum/app/Main.hs
@@ -1,54 +1,24 @@
 module Main (main) where
 
-import Data.Ord
-  ( comparing )
-import Data.List
-  ( sortBy
-  , subsequences )
 import qualified Data.Set as S
-  -- ( fromList, powerSet, map )
-import Data.Maybe
+import Data.List
+  ( sort )
 import Control.Monad
   ( replicateM
   , mapM_ )
-
--- -- The subsets of a given length and a given list
--- -- Ex: subsetsOf 2 [1,2,3] = [[1,2],[1,3],[2,3]]
--- subsetsOf :: (Eq t, Num t) => t -> [a] -> [[a]]
--- subsetsOf 0 _ = [[]]
--- subsetsOf _ [] = []
--- subsetsOf n (x : xs) = map (x :) (subsetsOf (n - 1) xs) ++ subsetsOf n xs
-
--- -- The subsets of a given list (in order) (excluding the empty set)
--- -- Ex: subsets [1,2,3] = [[1],[2],[3],[1,2],[1,3],[2,3],[1,2,3]]
--- subsets :: [a] -> [[a]]
--- subsets xs = subsets' 1 xs
---   where
---     -- A subfunction to encapsulate the n parameter
---     subsets' n xs
---       | n == length xs + 1 = []
---       | otherwise          = subsetsOf n xs <> subsets' (n + 1) xs
-
--- -- The length of the smallest subset in a given list that is equal to or greater than a given number or -1 if there are none
--- subsetSum :: (Ord p, Num p) => [p] -> p -> Int
--- subsetSum integers s = findSubset $ subsets integers
---   where
---     findSubset (set : sets)
---       | sum set >= s = length set
---       | otherwise    = findSubset sets
---     findSubset [] = -1
-
-subsets xs = S.powerSet $ S.fromList xs
-
-subsetSum xs s = fromMaybe (-1) $ S.lookupMin $ S.map S.size $ S.filter ((>= s) . S.foldl (+) 0) (S.powerSet $ S.fromList xs)
+import Data.Maybe
+  ( maybe )
 
 main :: IO ()
 main = do
   _ <- readLn :: IO Int -- Read but don't bind the upcoming list size
   integers <- (read <$>) . words <$> getLine :: IO [Int] -- Read and bind a list of integers
-  testCases <- readLn :: IO Int -- Read and bind the number of test cases to be entered
-  subsetSums <- replicateM testCases $ do -- For each testcase do the following and bind the list of results
-    s <- readLn :: IO Int -- Read and bind the integer to find whether a non-empty subset from integers is greater than or equal to
-    return $ subsetSum integers s -- Return the subset sum of the entered number and list of integers
+  let integerTotalsSet = S.fromList $ scanl (+) 0 $ reverse $ sort integers -- A set of totals when summing the given integers starting from the highest
+
+  numberOfTestCases <- readLn :: IO Int -- Read and bind the number of test cases to be entered
+
+  subsetSums <- replicateM numberOfTestCases $ do -- For each testcase do the following and bind the list of results
+    integer <- readLn :: IO Int -- Read and bind the integer to find whether a non-empty subset from integers is greater than or equal to
+    return $ maybe (- 1) (`S.findIndex` integerTotalsSet) (S.lookupGE integer integerTotalsSet) -- Return either -1 or the index of the lowest element greater than or equal to the given integer (if there exists one)
 
   mapM_ print subsetSums -- Print the subset sums of the entered list of integers and each entered number
