838a67db81daf50ed6739c417dfbacdd08822889
ChrisHobday <hobday.chris@protonmail.com>
2024-01-03 21:10:17 -0500

    Update BitterChocolate challenge, update main function to get input and output results, add winOrlose function to determine if player 1 can win with the given row sizes

--- Functional Programming/BitterChocolate/app/Main.hs
+++ Functional Programming/BitterChocolate/app/Main.hs
@@ -1,4 +1,15 @@
 module Main (main) where
 
+import Control.Monad
+  ( replicateM )
+
+winOrLose row1Size row2Size row3Size = ""
+
 main :: IO ()
-main = undefined
+main = do
+  numberOfTestCases <- readLn :: IO Int
+  testCaseResults <- replicateM numberOfTestCases $ do
+    (row1Size : row2Size : row3Size :_) <- (read <$>) . words <$> getLine :: IO [Int] -- Read and bind row sizes
+    return $ winOrLose row1Size row2Size row3Size
+  print testCaseResults
+
