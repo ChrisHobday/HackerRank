a33095f4d7613b789103e1ba4769148ea7335b00
ChrisHobday <hobday.chris@protonmail.com>
2024-01-08 23:12:58 -0500

    Update BitterChocolate challenge, add ChocolateBar datatype for representing a chocolate bar in the game to be played, add move function to simulate a move in the game to be played

--- Functional Programming/BitterChocolate/app/Main.hs
+++ Functional Programming/BitterChocolate/app/Main.hs
@@ -3,24 +3,32 @@ module Main (main) where
 import Control.Monad
   ( replicateM )
 
-winOrLose row1Size row2Size row3Size =
-  if row1Size == 0 then
-    if row2Size == 0 then
-      if row3Size == 0 then
-        "WIN"
-      else
-        if row3Size == 1 then
-          "LOSE"
-        else
-          "WIN"
-    else
-      if row3Size == 1 then
-        "WIN"
-      else
+data ChocolateBar =
+  ChocolateBar
+    { row1Length :: Int
+    , row2Length :: Int
+    , row3Length :: Int }
+
+move chocolateBar x y = undefined
+
+-- winOrLose row1Size row2Size row3Size =
+--   if row1Size == 0 then
+--     if row2Size == 0 then
+--       if row3Size == 0 then
+--         "WIN"
+--       else
+--         if row3Size == 1 then
+--           "LOSE"
+--         else
+--           "WIN"
+--     else
+--       if row3Size == 1 then
+--         "WIN"
+--       else
       
 
 
-  else
+--   else
 
 
 main :: IO ()
@@ -30,4 +38,3 @@ main = do
     (row1Size : row2Size : row3Size :_) <- (read <$>) . words <$> getLine :: IO [Int] -- Read and bind row sizes
     return $ winOrLose row1Size row2Size row3Size
   print testCaseResults
-
